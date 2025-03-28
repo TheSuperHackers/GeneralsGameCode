4486fb295809d24ad36e4045e28f33cd981c1678
Stephan Vedder <vedder@mbits.info>
2025-03-28 14:33:10 +0100

    [Compat] Fix ZH WWSaveLoad compilation
    Co-authored-by: Patrick Zacharias <1475802+Fighter19@users.noreply.github.com>

--- Dependencies/Utility/Utility/StringCompat.h
+++ Dependencies/Utility/Utility/StringCompat.h
@@ -4,6 +4,14 @@ typedef const char* LPCSTR;
 typedef char* LPSTR;
 
 // String functions
+inline char *_strlwr(char *str) {
+  for (int i = 0; str[i] != '\0'; i++) {
+    str[i] = tolower(str[i]);
+  }
+  return str;
+}
+
+#define strlwr _strlwr
 #define _vsnprintf vsnprintf
 #define _snprintf snprintf
 #define stricmp strcasecmp
