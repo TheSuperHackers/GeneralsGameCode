5ec2ac74a3e0fe51ed58ad1985aab52a480cd52c
Zdenek Zambersky <zzambers@users.noreply.github.com>
2025-03-30 18:58:57 +0200

    [ZH] Introdution of CPP_11 macro for code requiring c++11 or higher

--- Dependencies/Utility/Utility/CppMacros.h
+++ Dependencies/Utility/Utility/CppMacros.h
@@ -8,3 +8,9 @@
 #else
 #define NOEXCEPT_17
 #endif
+
+#if __cplusplus >= 201103L
+    #define CPP_11(code) code
+#else
+    #define CPP_11(code)
+#endif
--- GeneralsMD/Code/Libraries/Include/Lib/BaseType.h
+++ GeneralsMD/Code/Libraries/Include/Lib/BaseType.h
@@ -34,6 +34,7 @@
 
 #include <math.h>
 #include <string.h>
+#include <Utility/CppMacros.h>
 
 /*
 **	Turn off some unneeded warnings.
