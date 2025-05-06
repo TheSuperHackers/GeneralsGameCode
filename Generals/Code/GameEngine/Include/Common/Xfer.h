5738ea0c94509cdc02dff4715c6b596f3a38ba43
xezon <4720891+xezon@users.noreply.github.com>
2025-05-06 20:02:40 +0200

    [GEN] Unify code of GameEngine's Xfer (#821)

--- Generals/Code/GameEngine/Include/Common/Xfer.h
+++ Generals/Code/GameEngine/Include/Common/Xfer.h
@@ -43,7 +43,6 @@
 #define __XFER_H_
 
 // INCLUDES ///////////////////////////////////////////////////////////////////////////////////////
-#include "Common/ModelState.h"
 #include "Common/Science.h"
 #include "Common/Upgrade.h"
 
@@ -171,6 +170,7 @@ public:
 	virtual void xferRGBAColorInt( RGBAColorInt *rgbaColorInt );
 	virtual void xferObjectID( ObjectID *objectID );
 	virtual void xferDrawableID( DrawableID *drawableID );
+	virtual void xferSTLObjectIDVector( std::vector<ObjectID> *objectIDVectorData );
 	virtual void xferSTLObjectIDList( std::list< ObjectID > *objectIDListData );
 	virtual void xferSTLIntList( std::list< Int > *intListData );
 	virtual void xferScienceType( ScienceType *science );
