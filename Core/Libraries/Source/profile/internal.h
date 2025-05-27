d201ce991f0fbfe6fa11cf2c9c1a065c10b1bd03
Caball009 <82909616+Caball009@users.noreply.github.com>
2025-05-27 15:06:06 +0200

    Added explicit delete keyword to dummy constructors/operators.

--- Core/Libraries/Source/profile/internal.h
+++ Core/Libraries/Source/profile/internal.h
@@ -46,8 +46,8 @@
 
 class ProfileFastCS
 {
-  ProfileFastCS(const ProfileFastCS&);
-  ProfileFastCS& operator=(const ProfileFastCS&) { return *this; }
+  ProfileFastCS(const ProfileFastCS&) CPP_11(= delete);
+  ProfileFastCS& operator=(const ProfileFastCS&) CPP_11(= delete);
   
 	static HANDLE testEvent;
 
@@ -111,8 +111,8 @@ public:
 
 	class Lock
 	{
-    Lock(const Lock&);
-	Lock& operator=(const Lock&) { return *this; }
+    Lock(const Lock&) CPP_11(= delete);
+	Lock& operator=(const Lock&) CPP_11(= delete);
 
 		ProfileFastCS& CriticalSection;
 
