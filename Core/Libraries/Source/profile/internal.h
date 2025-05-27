db0f68cc9dafb2167461f23270d260d0b161b561
Caball009 <82909616+Caball009@users.noreply.github.com>
2025-05-27 15:04:29 +0200

    Added 'CppMacros.h' include.

--- Core/Libraries/Source/profile/internal.h
+++ Core/Libraries/Source/profile/internal.h
@@ -37,6 +37,7 @@
 #include "internal_highlevel.h"
 #include "internal_cmd.h"
 #include "internal_result.h"
+#include "Utility/CppMacros.h"
 
 #if !(defined(_MSC_VER) && _MSC_VER < 1300)
 #include <atomic>
