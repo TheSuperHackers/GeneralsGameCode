2d4c550c5cd2790b961518a47ceef3828218634c
xezon <4720891+xezon@users.noreply.github.com>
2025-05-29 11:43:32 +0200

    Remove debug ui message

--- Generals/Code/GameEngine/Source/GameClient/MessageStream/CommandXlat.cpp
+++ Generals/Code/GameEngine/Source/GameClient/MessageStream/CommandXlat.cpp
@@ -3137,8 +3137,6 @@ GameMessageDisposition CommandTranslator::translateGameMessage(const GameMessage
 			{
 				TheGameLogic->setGamePaused(FALSE);
 				TheGameLogic->setGamePausedInFrame(TheGameLogic->getFrame() + 1);
-				if (TheInGameUI)
-					TheInGameUI->message("Pause at frame %u", TheGameLogic->getFrame() + 1);
 			}
 			break;
 		}
