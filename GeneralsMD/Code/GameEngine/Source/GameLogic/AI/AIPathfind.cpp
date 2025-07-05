856ff89aa86de2e2e8dbdd0ea63c1b31e006f2e6
xezon <4720891+xezon@users.noreply.github.com>
2025-07-05 11:44:58 +0200

    Fix Pathfind logging for Zero Hour by hand

--- GeneralsMD/Code/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
@@ -5956,9 +5956,8 @@ void Pathfinder::processPathfindQueue(void)
 		timeToUpdate = ((double)(endTime64-startTime64) / (double)(freq64));
 		if (timeToUpdate>0.01f) 
 		{
-			DEBUG_LOG(("%d Pathfind queue: %d paths, %d cells", TheGameLogic->getFrame(), pathsFound, m_cumulativeCellsAllocated));
-			DEBUG_LOG(("Time %f (%f)", timeToUpdate, (::GetTickCount()-startTimeMS)/1000.0f));
-			DEBUG_LOG((""));
+			DEBUG_LOG(("%d Pathfind queue: %d paths, %d cells --", TheGameLogic->getFrame(), pathsFound, m_cumulativeCellsAllocated));
+			DEBUG_LOG(("time %f (%f)", timeToUpdate, (::GetTickCount()-startTimeMS)/1000.0f));
 		}
 #endif
 #endif
@@ -6719,8 +6718,7 @@ Path *Pathfinder::internalFindPath( Object *obj, const LocomotorSet& locomotorSe
 		Bool valid;
 		valid = validMovementPosition( isCrusher, obj->getLayer(), locomotorSet, to ) ;
 
-		DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-		DEBUG_LOG(("Pathfind failed from (%f,%f) to (%f,%f), OV %d", from->x, from->y, to->x, to->y, valid));
+		DEBUG_LOG(("%d Pathfind failed from (%f,%f) to (%f,%f), OV %d --", TheGameLogic->getFrame(), from->x, from->y, to->x, to->y, valid));
 		DEBUG_LOG(("Unit '%s', time %f, cells %d", obj->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f,cellCount));
 	}
 #endif
@@ -7363,8 +7361,7 @@ Path *Pathfinder::findGroundPath( const Coord3D *from,
 	}	
 #endif
 
-	DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-	DEBUG_LOG(("FindGroundPath failed from (%f,%f) to (%f,%f)", from->x, from->y, to->x, to->y));
+	DEBUG_LOG(("%d FindGroundPath failed from (%f,%f) to (%f,%f) --", TheGameLogic->getFrame(), from->x, from->y, to->x, to->y));
 	DEBUG_LOG(("time %f", (::GetTickCount()-startTimeMS)/1000.0f));
 
 #ifdef DUMP_PERF_STATS
@@ -7993,8 +7990,7 @@ Path *Pathfinder::internal_findHierarchicalPath( Bool isHuman, const LocomotorSu
 	}	
 #endif
 
-	DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-	DEBUG_LOG(("FindHierarchicalPath failed from (%f,%f) to (%f,%f)", from->x, from->y, to->x, to->y));
+	DEBUG_LOG(("%d FindHierarchicalPath failed from (%f,%f) to (%f,%f) --", TheGameLogic->getFrame(), from->x, from->y, to->x, to->y));
 	DEBUG_LOG(("time %f", (::GetTickCount()-startTimeMS)/1000.0f));
 
 #ifdef DUMP_PERF_STATS
@@ -8953,8 +8949,7 @@ Path *Pathfinder::findClosestPath( Object *obj, const LocomotorSet& locomotorSet
 			}
 #endif
 
-			DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-			DEBUG_LOG(("Pathfind(findClosestPath) chugged from (%f,%f) to (%f,%f), --", from->x, from->y, to->x, to->y));
+			DEBUG_LOG(("%d Pathfind(findClosestPath) chugged from (%f,%f) to (%f,%f) --", TheGameLogic->getFrame(), from->x, from->y, to->x, to->y));
 			DEBUG_LOG(("Unit '%s', time %f", obj->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 #ifdef INTENSE_DEBUG
 			TheScriptEngine->AppendDebugMessage("Big path FCP CC", false);
@@ -8979,8 +8974,7 @@ Path *Pathfinder::findClosestPath( Object *obj, const LocomotorSet& locomotorSet
 	Bool valid;
 	valid = validMovementPosition( isCrusher, obj->getLayer(), locomotorSet, to ) ;
 
-	DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-	DEBUG_LOG(("Pathfind(findClosestPath) failed from (%f,%f) to (%f,%f), original valid %d --", from->x, from->y, to->x, to->y, valid));
+	DEBUG_LOG(("Pathfind(findClosestPath) failed from (%f,%f) to (%f,%f), original valid %d --", TheGameLogic->getFrame(), from->x, from->y, to->x, to->y, valid));
 	DEBUG_LOG(("Unit '%s', time %f", obj->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 #endif
 #if defined RTS_DEBUG || defined RTS_INTERNAL
@@ -10395,8 +10389,7 @@ Path *Pathfinder::getMoveAwayFromPath(Object* obj, Object *otherObj,
 	debugShowSearch(true);
 #endif
 
-	DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-	DEBUG_LOG(("getMoveAwayFromPath pathfind failed  -- "));
+	DEBUG_LOG(("%d getMoveAwayFromPath pathfind failed --", TheGameLogic->getFrame()));
 	DEBUG_LOG(("Unit '%s', time %f", obj->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 
 	m_isTunneling = false;
@@ -10574,8 +10567,7 @@ Path *Pathfinder::patchPath( const Object *obj, const LocomotorSet& locomotorSet
 		}
 	}
 
-	DEBUG_LOG(("%d ", TheGameLogic->getFrame()));
-	DEBUG_LOG(("patchPath Pathfind failed  -- "));
+	DEBUG_LOG(("%d patchPath Pathfind failed --", TheGameLogic->getFrame()));
 	DEBUG_LOG(("Unit '%s', time %f", obj->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 
 #if defined RTS_DEBUG || defined RTS_INTERNAL
@@ -10933,8 +10925,7 @@ Path *Pathfinder::findAttackPath( const Object *obj, const LocomotorSet& locomot
 		return path;
 	}
 #if defined RTS_DEBUG || defined RTS_INTERNAL
-	DEBUG_LOG(("%d (%d cells)", TheGameLogic->getFrame(), cellCount));
-	DEBUG_LOG(("Attack Pathfind failed from (%f,%f) to (%f,%f) -- ", from->x, from->y, victim->getPosition()->x, victim->getPosition()->y));
+	DEBUG_LOG(("%d (%d cells) Attack Pathfind failed from (%f,%f) to (%f,%f) --", TheGameLogic->getFrame(), cellCount, from->x, from->y, victim->getPosition()->x, victim->getPosition()->y));
 	DEBUG_LOG(("Unit '%s', attacking '%s' time %f", obj->getTemplate()->getName().str(),  victim->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 #endif
 #endif
@@ -11094,8 +11085,7 @@ Path *Pathfinder::findSafePath( const Object *obj, const LocomotorSet& locomotor
 #endif
 #if 0
 #if defined RTS_DEBUG || defined RTS_INTERNAL
-	DEBUG_LOG(("%d (%d cells)", TheGameLogic->getFrame(), cellCount));
-	DEBUG_LOG(("Attack Pathfind failed from (%f,%f) to (%f,%f) -- ", from->x, from->y, victim->getPosition()->x, victim->getPosition()->y));
+	DEBUG_LOG(("%d (%d cells) Attack Pathfind failed from (%f,%f) to (%f,%f) --", TheGameLogic->getFrame(), cellCount, from->x, from->y, victim->getPosition()->x, victim->getPosition()->y));
 	DEBUG_LOG(("Unit '%s', attacking '%s' time %f", obj->getTemplate()->getName().str(),  victim->getTemplate()->getName().str(), (::GetTickCount()-startTimeMS)/1000.0f));
 #endif
 #endif
