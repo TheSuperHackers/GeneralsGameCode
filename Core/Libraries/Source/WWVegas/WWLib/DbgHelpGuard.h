1e9d67cdb3bee5bc8c95730cbf19e69a485a4a7d
xezon <4720891+xezon@users.noreply.github.com>
2025-07-05 20:23:56 +0200

    Update comments

--- Core/Libraries/Source/WWVegas/WWLib/DbgHelpGuard.h
+++ Core/Libraries/Source/WWVegas/WWLib/DbgHelpGuard.h
@@ -24,7 +24,7 @@
 // This class temporarily loads and unloads dbghelp.dll from the desired location to prevent
 // other code from potentially loading it from an undesired location.
 // This helps avoid crashing on boot using recent AMD/ATI drivers, which attempt to load and use
-// dbghelp.dll from the game install directory but are unable to do so correctly because
+// dbghelp.dll from the game install directory but are unable to do so without crashing because
 // the dbghelp.dll that ships with the game is very old and the AMD/ATI code does not handle
 // that correctly.
 
