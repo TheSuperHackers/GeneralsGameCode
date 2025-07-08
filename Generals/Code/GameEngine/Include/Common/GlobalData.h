TheSuperHackers/GeneralsGameCode

commit 831579b411e85542ce12cd1e76b8743c5eb3534b

[GEN][ZH] Remove all RTS_INTERNAL epilogues (#1231)

Generals/Code/GameEngine/Source/Common/CommandLine.cpp
@@ -37,11 +37,6 @@
 #include "GameNetwork/NetworkDefs.h"
 #include "trim.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 
 

Generals/Code/GameEngine/Source/Common/GameEngine.cpp
@@ -110,11 +110,6 @@
 
 #include "Common/version.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 //-------------------------------------------------------------------------------------------------
 

Generals/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngine.cpp
@@ -56,11 +56,6 @@
 #include "GameLogic/ScriptEngine.h"
 #include "GameLogic/SidesList.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 // These are for debugger window
 static int st_LastCurrentFrame;

GeneralsMD/Code/GameEngine/Source/Common/CommandLine.cpp
@@ -37,11 +37,6 @@
 #include "GameNetwork/NetworkDefs.h"
 #include "trim.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 
 

GeneralsMD/Code/GameEngine/Source/Common/GameEngine.cpp
@@ -108,11 +108,6 @@
 
 #include "Common/version.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 //-------------------------------------------------------------------------------------------------
 

GeneralsMD/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngine.cpp
@@ -57,11 +57,6 @@
 #include "GameLogic/ScriptEngine.h"
 #include "GameLogic/SidesList.h"
 
-#ifdef RTS_INTERNAL
-// for occasional debugging...
-//#pragma optimize("", off)
-//#pragma MESSAGE("************************************** WARNING, optimization disabled for debugging purposes")
-#endif
 
 // These are for debugger window
 static int st_LastCurrentFrame;

commit 5bb11b569ce9ded954037276104f9699aea0600d

[GEN][ZH] Fix VTune GameDataDebug.ini field parse error introduced by #1231

Generals/Code/GameEngine/Include/Common/GlobalData.h
@@ -465,10 +465,8 @@ public:
   Int					m_playStats;									///< Int whether we want to log play stats or not, if <= 0 then we don't log
 
   Bool m_TiVOFastMode;            ///< When true, the client speeds up the framerate... set by HOTKEY!
+  
 
-#if defined(RTS_PROFILE)
-	Bool m_vTune;
-#endif
 
 #if defined(RTS_DEBUG)
 	Bool m_wireframe;
@@ -485,6 +483,7 @@ public:
 	Bool m_disableMilitaryCaption;					///< if true, military briefings go fast
 	Int m_benchmarkTimer;										///< how long to play the game in benchmark mode?
 	Bool m_checkForLeaks;
+	Bool m_vTune;
 	Bool m_debugCamera;						///< Used to display Camera debug information
 	Bool m_debugVisibility;						///< Should we actively debug the visibility
 	Int m_debugVisibilityTileCount;		///< How many tiles we should show when debugging visibility

Generals/Code/GameEngine/Source/Common/CommandLine.cpp
@@ -697,10 +697,6 @@ Int parseLogAssets( char *args[], int num )
 	return 1;
 }
 
-#endif // defined(RTS_DEBUG)
-
-#if defined(RTS_PROFILE)
-
 /// begin stuff for VTUNE
 Int parseVTune ( char *args[], int num )
 {
@@ -710,7 +706,7 @@ Int parseVTune ( char *args[], int num )
 }
 /// end stuff for VTUNE
 
-#endif // defined(RTS_PROFILE)
+#endif // defined(RTS_DEBUG)
 
 //=============================================================================
 //=============================================================================
@@ -1278,6 +1274,7 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-DemoLoadScreen", parseDemoLoadScreen },
 	{ "-cameraDebug", parseCameraDebug },
 	{ "-logToCon", parseLogToConsole },
+	{ "-vTune", parseVTune },
 	{ "-selectTheUnselectable", parseSelectAll },
 	{ "-RunAhead", parseRunAhead },
 	{ "-noshroud", parseNoShroud },
@@ -1300,10 +1297,6 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-showTeamDot", parseShowTeamDot },
 	{ "-extraLogging", parseExtraLogging },
 
-#endif
-
-#if defined(RTS_PROFILE)
-	{ "-vTune", parseVTune },
 #endif
 
 #ifdef DEBUG_LOGGING

Generals/Code/GameEngine/Source/Common/GameEngine.cpp
@@ -305,7 +305,7 @@ void GameEngine::init()
 
 
 	#if defined(RTS_DEBUG)
-		// If we're in Debug or Internal, load the Debug info as well.
+		// If we're in Debug, load the Debug settings as well.
 		ini.load( AsciiString( "Data\\INI\\GameDataDebug.ini" ), INI_LOAD_OVERWRITE, NULL );
 	#endif
 		

Generals/Code/GameEngine/Source/Common/GlobalData.cpp
@@ -488,10 +488,6 @@ GlobalData* GlobalData::m_theOriginal = NULL;
 	{ "KeyboardCameraRotateSpeed", INI::parseReal, NULL, offsetof( GlobalData, m_keyboardCameraRotateSpeed ) },
 	{ "PlayStats",									INI::parseInt,				NULL,			offsetof( GlobalData, m_playStats ) },
 
-#if defined(RTS_PROFILE)
-	{ "VTune", INI::parseBool,	NULL,			offsetof( GlobalData, m_vTune ) },
-#endif
-
 #if defined(RTS_DEBUG)
 	{ "DisableCameraFade",			INI::parseBool,				NULL,			offsetof( GlobalData, m_disableCameraFade ) },
 	{ "DisableScriptedInputDisabling",			INI::parseBool,		NULL,			offsetof( GlobalData, m_disableScriptedInputDisabling ) },
@@ -518,6 +514,7 @@ GlobalData* GlobalData::m_theOriginal = NULL;
 	{ "MaxDebugThreatMapValue",							INI::parseUnsignedInt,			NULL,	offsetof( GlobalData, m_maxDebugThreat) },
 	{ "DebugCashValueMapTileDuration",			INI::parseInt,							NULL,	offsetof( GlobalData, m_debugCashValueMapTileDuration) },
 	{ "MaxDebugCashValueMapValue",					INI::parseUnsignedInt,			NULL,	offsetof( GlobalData, m_maxDebugValue) },
+	{ "VTune", INI::parseBool,	NULL,			offsetof( GlobalData, m_vTune ) },
 	{ "SaveStats",									INI::parseBool,				NULL,			offsetof( GlobalData, m_saveStats ) },
 	{ "UseLocalMOTD",								INI::parseBool,				NULL,			offsetof( GlobalData, m_useLocalMOTD ) },
 	{ "BaseStatsDir",								INI::parseAsciiString,NULL,			offsetof( GlobalData, m_baseStatsDir ) },
@@ -548,10 +545,6 @@ GlobalData::GlobalData()
 
   m_TiVOFastMode = FALSE;
 
-#if defined(RTS_PROFILE)
-	m_vTune = false;
-#endif
-
 #if defined(RTS_DEBUG)
 	m_wireframe = 0;
 	m_stateMachineDebug = FALSE;
@@ -576,6 +569,7 @@ GlobalData::GlobalData()
 	m_debugCashValueMap = FALSE;
 	m_maxDebugValue = 10000;
 	m_debugCashValueMapTileDuration = LOGICFRAMES_PER_SECOND; // Changed By Sadullah Nader
+	m_vTune = false;
 	m_checkForLeaks = TRUE;
 	m_benchmarkTimer = -1;
 	m_allowUnselectableSelection = FALSE;

Generals/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngine.cpp
@@ -99,7 +99,7 @@ Bool st_particleSystemNeedsStopping = FALSE; ///< Set along with st_particleSyst
 #define FORMAT_STRING_LEADING_STRING		"%s%.2f"
 // That's it for particle editor
 
-#if defined(RTS_PROFILE)
+#if defined(RTS_DEBUG)
 	#define DO_VTUNE_STUFF
 #endif
 

GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
@@ -480,10 +480,8 @@ public:
 	Bool m_specialPowerUsesDelay ;
 #endif
   Bool m_TiVOFastMode;            ///< When true, the client speeds up the framerate... set by HOTKEY!
+  
 
-#if defined(RTS_PROFILE)
-	Bool m_vTune;
-#endif
 
 #if defined(RTS_DEBUG)
 	Bool m_wireframe;
@@ -499,6 +497,7 @@ public:
 	Bool m_disableMilitaryCaption;					///< if true, military briefings go fast
 	Int m_benchmarkTimer;										///< how long to play the game in benchmark mode?
   Bool m_checkForLeaks;
+	Bool m_vTune;
 	Bool m_debugCamera;						///< Used to display Camera debug information
 	Bool m_debugVisibility;						///< Should we actively debug the visibility
 	Int m_debugVisibilityTileCount;		///< How many tiles we should show when debugging visibility

GeneralsMD/Code/GameEngine/Source/Common/CommandLine.cpp
@@ -697,10 +697,6 @@ Int parseLogAssets( char *args[], int num )
 	return 1;
 }
 
-#endif // defined(RTS_DEBUG)
-
-#if defined(RTS_PROFILE)
-
 /// begin stuff for VTUNE
 Int parseVTune ( char *args[], int num )
 {
@@ -710,7 +706,7 @@ Int parseVTune ( char *args[], int num )
 }
 /// end stuff for VTUNE
 
-#endif // defined(RTS_PROFILE)
+#endif // defined(RTS_DEBUG)
 
 //=============================================================================
 //=============================================================================
@@ -1278,6 +1274,7 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-DemoLoadScreen", parseDemoLoadScreen },
 	{ "-cameraDebug", parseCameraDebug },
 	{ "-logToCon", parseLogToConsole },
+	{ "-vTune", parseVTune },
 	{ "-selectTheUnselectable", parseSelectAll },
 	{ "-RunAhead", parseRunAhead },
 	{ "-noshroud", parseNoShroud },
@@ -1300,10 +1297,6 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-showTeamDot", parseShowTeamDot },
 	{ "-extraLogging", parseExtraLogging },
 
-#endif
-
-#if defined(RTS_PROFILE)
-	{ "-vTune", parseVTune },
 #endif
 
 #ifdef DEBUG_LOGGING

GeneralsMD/Code/GameEngine/Source/Common/GameEngine.cpp
@@ -376,7 +376,7 @@ void GameEngine::init()
 
 
 	#if defined(RTS_DEBUG)
-		// If we're in Debug or Internal, load the Debug info as well.
+		// If we're in Debug, load the Debug settings as well.
 		ini.load( AsciiString( "Data\\INI\\GameDataDebug.ini" ), INI_LOAD_OVERWRITE, NULL );
 	#endif
 		

GeneralsMD/Code/GameEngine/Source/Common/GlobalData.cpp
@@ -488,10 +488,6 @@ GlobalData* GlobalData::m_theOriginal = NULL;
 	{ "KeyboardCameraRotateSpeed", INI::parseReal, NULL, offsetof( GlobalData, m_keyboardCameraRotateSpeed ) },
 	{ "PlayStats",									INI::parseInt,				NULL,			offsetof( GlobalData, m_playStats ) },
 
-#if defined(RTS_PROFILE)
-	{ "VTune", INI::parseBool,	NULL,			offsetof( GlobalData, m_vTune ) },
-#endif
-
 #if defined(RTS_DEBUG)
 	{ "DisableCameraFade",			INI::parseBool,				NULL,			offsetof( GlobalData, m_disableCameraFade ) },
 	{ "DisableScriptedInputDisabling",			INI::parseBool,		NULL,			offsetof( GlobalData, m_disableScriptedInputDisabling ) },
@@ -518,6 +514,7 @@ GlobalData* GlobalData::m_theOriginal = NULL;
 	{ "MaxDebugThreatMapValue",							INI::parseUnsignedInt,			NULL,	offsetof( GlobalData, m_maxDebugThreat) },
 	{ "DebugCashValueMapTileDuration",			INI::parseInt,							NULL,	offsetof( GlobalData, m_debugCashValueMapTileDuration) },
 	{ "MaxDebugCashValueMapValue",					INI::parseUnsignedInt,			NULL,	offsetof( GlobalData, m_maxDebugValue) },
+	{ "VTune", INI::parseBool,	NULL,			offsetof( GlobalData, m_vTune ) },
 	{ "SaveStats",									INI::parseBool,				NULL,			offsetof( GlobalData, m_saveStats ) },
 	{ "UseLocalMOTD",								INI::parseBool,				NULL,			offsetof( GlobalData, m_useLocalMOTD ) },
 	{ "BaseStatsDir",								INI::parseAsciiString,NULL,			offsetof( GlobalData, m_baseStatsDir ) },
@@ -551,10 +548,6 @@ GlobalData::GlobalData()
 #endif
   m_TiVOFastMode = FALSE;
 
-#if defined(RTS_PROFILE)
-	m_vTune = false;
-#endif
-
 #if defined(RTS_DEBUG)
 	m_wireframe = 0;
 	m_stateMachineDebug = FALSE;
@@ -578,6 +571,7 @@ GlobalData::GlobalData()
 	m_debugCashValueMap = FALSE;
 	m_maxDebugValue = 10000;
 	m_debugCashValueMapTileDuration = LOGICFRAMES_PER_SECOND; // Changed By Sadullah Nader
+	m_vTune = false;
 	m_checkForLeaks = TRUE;
 	m_benchmarkTimer = -1;
   

GeneralsMD/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngine.cpp
@@ -100,7 +100,7 @@ Bool st_particleSystemNeedsStopping = FALSE; ///< Set along with st_particleSyst
 #define FORMAT_STRING_LEADING_STRING		"%s%.2f"
 // That's it for particle editor
 
-#if defined(RTS_PROFILE)
+#if defined(RTS_DEBUG)
 	#define DO_VTUNE_STUFF
 #endif
 