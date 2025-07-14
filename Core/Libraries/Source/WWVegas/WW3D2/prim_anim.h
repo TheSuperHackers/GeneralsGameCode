32354e00a42104517ffb1f236ff8b3ac9de00c37
Helmut Buhler <buhler@8gadgetpack.net>
2025-07-14 20:06:14 +0200

    Simplify typename hack

--- Core/Libraries/Source/WWVegas/WW3D2/prim_anim.h
+++ Core/Libraries/Source/WWVegas/WW3D2/prim_anim.h
@@ -45,14 +45,6 @@
 #include "simplevec.h"
 #include "chunkio.h"
 
-// TheSuperHackers VS 2019 and earlier require a hack in order to compile this
-#if defined(_MSC_VER) && _MSC_VER <= 1929
-#define TYPENAME_HACK1 typename
-#define TYPENAME_HACK2
-#else
-#define TYPENAME_HACK1
-#define TYPENAME_HACK2 typename
-#endif
 
 // Forward declarations
 class ChunkSaveClass;
@@ -171,7 +163,7 @@ class LERPAnimationChannelClass : public PrimitiveAnimationChannelClass<T>
 	using PrimitiveAnimationChannelClass<T>::m_Data;
 	using PrimitiveAnimationChannelClass<T>::m_LastIndex;
 public:
-	using TYPENAME_HACK2 PrimitiveAnimationChannelClass<T>::KeyClass;
+	using typename PrimitiveAnimationChannelClass<T>::KeyClass;
 
 public:
 
@@ -195,7 +187,7 @@ int PrimitiveAnimationChannelClass<T>::Get_Key_Count (void) const
 //	Set_Key_Value
 /////////////////////////////////////////////////////////
 template<class T>
-TYPENAME_HACK1 const PrimitiveAnimationChannelClass<T>::KeyClass &PrimitiveAnimationChannelClass<T>::Get_Key (int index) const
+const typename PrimitiveAnimationChannelClass<T>::KeyClass &PrimitiveAnimationChannelClass<T>::Get_Key (int index) const
 {
 	return m_Data[index];
 }
@@ -395,8 +387,5 @@ LERPAnimationChannelClass<T>::Evaluate (float time)
 
 	return value;
 }
-
-#undef TYPENAME_HACK1
-#undef TYPENAME_HACK2
 
 #endif //__PRIM_ANIM_H
