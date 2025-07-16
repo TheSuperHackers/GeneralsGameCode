TheSuperHackers/GeneralsGameCode

commit d8af372439acb45cd37f62441b8eefa6f82d0f2a

[GEN][ZH] Fix heap-use-after-free in W3DRadar::drawHeroIcon() (#1035)

GeneralsMD/Code/GameEngine/Include/Common/Radar.h
@@ -190,9 +190,8 @@ public:
  	Bool tryEvent( RadarEventType event, const Coord3D *pos );	///< try to make a "stealth" event
 
 	// adding and removing objects from the radar
-	void addObject( Object *obj );													///< add object to radar
-	void removeObject( Object *obj );												///< remove object from radar
-	void examineObject( Object *obj );											///< re-examine object and resort if needed
+	virtual bool addObject( Object *obj );									///< add object to radar
+	virtual bool removeObject( Object *obj );								///< remove object from radar
 
 	// radar options
 	void hide( Bool hide ) { m_radarHidden = hide; }				///< hide/unhide the radar

GeneralsMD/Code/GameEngine/Source/Common/System/Radar.cpp
@@ -397,13 +397,13 @@ void Radar::newMap( TerrainLogic *terrain )
 /** Add an object to the radar list.  The object will be sorted in the list to be grouped
 	* using it's radar priority */
 //-------------------------------------------------------------------------------------------------
-void Radar::addObject( Object *obj )
+bool Radar::addObject( Object *obj )
 {
 
 	// get the radar priority for this object
 	RadarPriorityType newPriority = obj->getRadarPriority();
 	if( isPriorityVisible( newPriority ) == FALSE )
-		return;
+		return false;
 
 	// if this object is on the radar, remove it in favor of the new add
 	RadarObject **list;
@@ -547,6 +547,7 @@ void Radar::addObject( Object *obj )
 
 	}  // end else
 
+	return true;
 }  // end addObject
 
 //-------------------------------------------------------------------------------------------------
@@ -593,24 +594,24 @@ Bool Radar::deleteFromList( Object *obj, RadarObject **list )
 //-------------------------------------------------------------------------------------------------
 /** Remove an object from the radar, the object may reside in any list */
 //-------------------------------------------------------------------------------------------------
-void Radar::removeObject( Object *obj )
+bool Radar::removeObject( Object *obj )
 {
 
 	// sanity
 	if( obj->friend_getRadarData() == NULL )
-		return;
+		return false;
 
 	if( deleteFromList( obj, &m_localObjectList ) == TRUE )
-		return;
+		return true;
 	else if( deleteFromList( obj, &m_objectList ) == TRUE )
-		return;
+		return true;
 	else
 	{
 
 		// sanity
 		DEBUG_ASSERTCRASH( 0, ("Radar: Tried to remove object '%s' which was not found\n",
 											 obj->getTemplate()->getName().str()) );
-
+		return false;
 	}  // end else
 
 }  // end removeObject

GeneralsMD/Code/GameEngineDevice/Include/W3DDevice/Common/W3DRadar.h
@@ -59,6 +59,9 @@ public:
 	virtual void update( void );																	///< subsystem update
 	virtual void reset( void );																		///< subsystem reset
 
+	virtual bool addObject( Object *obj );									///< add object to radar
+	virtual bool removeObject( Object *obj );								///< remove object from radar
+
 	virtual void newMap( TerrainLogic *terrain );				///< reset radar for new map
 
 	void draw( Int pixelX, Int pixelY, Int width, Int height );		///< draw the radar
@@ -80,7 +83,7 @@ protected:
 	void drawViewBox( Int pixelX, Int pixelY, Int width, Int height );  ///< draw view box
 	void buildTerrainTexture( TerrainLogic *terrain );	 ///< create the terrain texture of the radar
 	void drawIcons( Int pixelX, Int pixelY, Int width, Int height );	///< draw all of the radar icons
-	void renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero = FALSE );			 ///< render an object list to the texture
+	void renderObjectList( const RadarObject *listHead, TextureClass *texture );			 ///< render an object list to the texture
 	void interpolateColorForHeight( RGBColor *color, 
 																	Real height, 
 																	Real hiZ, 
@@ -118,7 +121,7 @@ protected:
 	Real m_viewZoom;															///< camera zoom used for the view box we have	
 	ICoord2D m_viewBox[ 4 ];											///< radar cell points for the 4 corners of view box
 
-	std::list<const Coord3D *> m_cachedHeroPosList;					//< cache of hero positions for drawing icons in radar overlay
+	std::vector<const Object *> m_cachedHeroObjectList; //< cache of hero objects for drawing icons in radar overlay
 };
 
 

GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/Common/System/W3DRadar.cpp
@@ -606,10 +606,10 @@ void W3DRadar::drawEvents( Int pixelX, Int pixelY, Int width, Int height )
 void W3DRadar::drawIcons( Int pixelX, Int pixelY, Int width, Int height )
 {
 	// draw the hero icons
-	std::list<const Coord3D *>::const_iterator iter = m_cachedHeroPosList.begin();
-	while (iter != m_cachedHeroPosList.end())
+	std::vector<const Object *>::const_iterator iter = m_cachedHeroObjectList.begin();
+	while (iter != m_cachedHeroObjectList.end())
 	{
-		drawHeroIcon( pixelX, pixelY, width, height, (*iter) );
+		drawHeroIcon( pixelX, pixelY, width, height, (*iter)->getPosition() );
 		++iter;
 	}
 }
@@ -617,7 +617,7 @@ void W3DRadar::drawIcons( Int pixelX, Int pixelY, Int width, Int height )
 //-------------------------------------------------------------------------------------------------
 /** Render an object list into the texture passed in */
 //-------------------------------------------------------------------------------------------------
-void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero )
+void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture )
 {
 
 	// sanity
@@ -635,12 +635,6 @@ void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *text
 	if (player)
 		playerIndex=player->getPlayerIndex();
 
-	if( calcHero )
-	{
-		// clear all entries from the cached hero object list
-		m_cachedHeroPosList.clear();
-	}
-
 	for( const RadarObject *rObj = listHead; rObj; rObj = rObj->friend_getNext() )
 	{
 
@@ -650,11 +644,6 @@ void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *text
 		// get object
 		const Object *obj = rObj->friend_getObject();
 
-		// cache hero object positions for drawing in icon layer
-		if( calcHero && obj->isHero() )
-		{
-			m_cachedHeroPosList.push_back(obj->getPosition());
-		}
     Bool skip = FALSE;
 
 		// check for shrouded status
@@ -994,6 +983,34 @@ void W3DRadar::update( void )
 	Radar::update();
 
 }  // end update
+
+//-------------------------------------------------------------------------------------------------
+bool W3DRadar::addObject( Object *obj )
+{
+	if (Radar::addObject(obj))
+	{
+		if (obj->isHero())
+		{
+			m_cachedHeroObjectList.push_back(obj);
+		}
+		return true;
+	}
+	return false;
+}
+
+//-------------------------------------------------------------------------------------------------
+bool W3DRadar::removeObject( Object *obj )
+{
+	if (Radar::removeObject(obj))
+	{
+		if (obj->isHero())
+		{
+			stl::find_and_erase_unordered(m_cachedHeroObjectList, obj);
+		}
+		return true;
+	}
+	return false;
+}
 
 //-------------------------------------------------------------------------------------------------
 /** Reset the radar for the new map data being given to it */
@@ -1404,7 +1421,7 @@ void W3DRadar::draw( Int pixelX, Int pixelY, Int width, Int height )
 
 		// rebuild the object overlay
 		renderObjectList( getObjectList(), m_overlayTexture );
-		renderObjectList( getLocalObjectList(), m_overlayTexture, TRUE );
+		renderObjectList( getLocalObjectList(), m_overlayTexture );
 		
 	}  // end if
 
@@ -1463,7 +1480,7 @@ void W3DRadar::refreshTerrain( TerrainLogic *terrain )
 
 /*
  *
-	void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero )
+	void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture )
 {
 
 	// sanity
@@ -1483,12 +1500,6 @@ void W3DRadar::refreshTerrain( TerrainLogic *terrain )
 
 	UnsignedByte minAlpha = 8;
 
-	if( calcHero )
-	{
-		// clear all entries from the cached hero object list
-		m_cachedHeroPosList.clear();
-	}
-
 	for( const RadarObject *rObj = listHead; rObj; rObj = rObj->friend_getNext() )
 	{
     UnsignedByte h = (UnsignedByte)(rObj->isTemporarilyHidden());
@@ -1500,12 +1511,6 @@ void W3DRadar::refreshTerrain( TerrainLogic *terrain )
 		// get object
 		const Object *obj = rObj->friend_getObject();
 		UnsignedByte r = 1;   // all decoys
-
-		// cache hero object positions for drawing in icon layer
-		if( calcHero && obj->isHero() )
-		{
-			m_cachedHeroPosList.push_back(obj->getPosition());
-		}
 
 		// get the color we're going to draw in
 		UnsignedInt c = 0xfe000000;// this is a decoy

Generals/Code/GameEngine/Include/Common/Radar.h
@@ -190,9 +190,8 @@ public:
  	Bool tryEvent( RadarEventType event, const Coord3D *pos );	///< try to make a "stealth" event
 
 	// adding and removing objects from the radar
-	void addObject( Object *obj );													///< add object to radar
-	void removeObject( Object *obj );												///< remove object from radar
-	void examineObject( Object *obj );											///< re-examine object and resort if needed
+	virtual bool addObject( Object *obj );									///< add object to radar
+	virtual bool removeObject( Object *obj );								///< remove object from radar
 
 	// radar options
 	void hide( Bool hide ) { m_radarHidden = hide; }				///< hide/unhide the radar

Generals/Code/GameEngine/Source/Common/System/Radar.cpp
@@ -394,13 +394,13 @@ void Radar::newMap( TerrainLogic *terrain )
 /** Add an object to the radar list.  The object will be sorted in the list to be grouped
 	* using it's radar priority */
 //-------------------------------------------------------------------------------------------------
-void Radar::addObject( Object *obj )
+bool Radar::addObject( Object *obj )
 {
 
 	// get the radar priority for this object
 	RadarPriorityType newPriority = obj->getRadarPriority();
 	if( isPriorityVisible( newPriority ) == FALSE )
-		return;
+		return false;
 
 	// if this object is on the radar, remove it in favor of the new add
 	RadarObject **list;
@@ -544,6 +544,7 @@ void Radar::addObject( Object *obj )
 
 	}  // end else
 
+	return true;
 }  // end addObject
 
 //-------------------------------------------------------------------------------------------------
@@ -590,24 +591,24 @@ Bool Radar::deleteFromList( Object *obj, RadarObject **list )
 //-------------------------------------------------------------------------------------------------
 /** Remove an object from the radar, the object may reside in any list */
 //-------------------------------------------------------------------------------------------------
-void Radar::removeObject( Object *obj )
+bool Radar::removeObject( Object *obj )
 {
 
 	// sanity
 	if( obj->friend_getRadarData() == NULL )
-		return;
+		return false;
 
 	if( deleteFromList( obj, &m_localObjectList ) == TRUE )
-		return;
+		return true;
 	else if( deleteFromList( obj, &m_objectList ) == TRUE )
-		return;
+		return true;
 	else
 	{
 
 		// sanity
 		DEBUG_ASSERTCRASH( 0, ("Radar: Tried to remove object '%s' which was not found\n",
 											 obj->getTemplate()->getName().str()) );
-
+		return false;
 	}  // end else
 
 }  // end removeObject

Generals/Code/GameEngineDevice/Include/W3DDevice/Common/W3DRadar.h
@@ -59,6 +59,9 @@ public:
 	virtual void update( void );																	///< subsystem update
 	virtual void reset( void );																		///< subsystem reset
 
+	virtual bool addObject( Object *obj );									///< add object to radar
+	virtual bool removeObject( Object *obj );								///< remove object from radar
+
 	virtual void newMap( TerrainLogic *terrain );				///< reset radar for new map
 
 	void draw( Int pixelX, Int pixelY, Int width, Int height );		///< draw the radar
@@ -80,7 +83,7 @@ protected:
 	void drawViewBox( Int pixelX, Int pixelY, Int width, Int height );  ///< draw view box
 	void buildTerrainTexture( TerrainLogic *terrain );	 ///< create the terrain texture of the radar
 	void drawIcons( Int pixelX, Int pixelY, Int width, Int height );	///< draw all of the radar icons
-	void renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero = FALSE );			 ///< render an object list to the texture
+	void renderObjectList( const RadarObject *listHead, TextureClass *texture );			 ///< render an object list to the texture
 	void interpolateColorForHeight( RGBColor *color, 
 																	Real height, 
 																	Real hiZ, 
@@ -118,7 +121,7 @@ protected:
 	Real m_viewZoom;															///< camera zoom used for the view box we have	
 	ICoord2D m_viewBox[ 4 ];											///< radar cell points for the 4 corners of view box
 
-	std::list<const Coord3D *> m_cachedHeroPosList;					//< cache of hero positions for drawing icons in radar overlay
+	std::vector<const Object *> m_cachedHeroObjectList; //< cache of hero objects for drawing icons in radar overlay
 };
 
 

commit 5b7684ee9aab95dd943135abe024db434854bc59

[ZH] Fix Hero Radar icon garrison issues introduced by #1035

GeneralsMD/Code/GameEngine/Include/Common/Radar.h
@@ -80,6 +80,13 @@ enum RadarEventType CPP_11(: Int)
  	
 };
 
+enum RadarObjectType CPP_11(: Int)
+{
+	RadarObjectType_None = 0,
+	RadarObjectType_Regular,
+	RadarObjectType_Local,
+};
+
 // PROTOTYPES /////////////////////////////////////////////////////////////////////////////////////
 
 //-------------------------------------------------------------------------------------------------
@@ -109,6 +116,7 @@ public:
 	inline const RadarObject *friend_getNext( void ) const { return m_next; }
 
 	Bool isTemporarilyHidden() const;
+	static Bool isTemporarilyHidden(const Object* obj);
 
 protected:
 
@@ -178,7 +186,7 @@ public:
 													ICoord2D *ul, ICoord2D *lr );					///< make translation for screen area of radar square to scaled aspect ratio preserving points inside the radar area
 
 	// priority inquiry
-	Bool isPriorityVisible( RadarPriorityType priority ) const;		///< is the priority passed in a "visible" one on the radar
+	static Bool isPriorityVisible( RadarPriorityType priority );		///< is the priority passed in a "visible" one on the radar
 
 	// radar events
 	void createEvent( const Coord3D *world, RadarEventType type, Real secondsToLive = 4.0f );	///< create radar event at location in world
@@ -190,8 +198,8 @@ public:
  	Bool tryEvent( RadarEventType event, const Coord3D *pos );	///< try to make a "stealth" event
 
 	// adding and removing objects from the radar
-	virtual bool addObject( Object *obj );									///< add object to radar
-	virtual bool removeObject( Object *obj );								///< remove object from radar
+	virtual RadarObjectType addObject( Object *obj );									///< add object to radar
+	virtual RadarObjectType removeObject( Object *obj );								///< remove object from radar
 
 	// radar options
 	void hide( Bool hide ) { m_radarHidden = hide; }				///< hide/unhide the radar
@@ -225,9 +233,6 @@ protected:
 	virtual void crc( Xfer *xfer );
 	virtual void xfer( Xfer *xfer );
 	virtual void loadPostProcess( void );
-
-	virtual void onLocalRadarObjectAdded( const RadarObject* radarObject ) = 0;
-	virtual void onLocalRadarObjectRemoved( const RadarObject* radarObject ) = 0;
 
 	/// internal method for creating a radar event with specific colors
 	void internalCreateEvent( const Coord3D *world, RadarEventType type, Real secondsToLive,
@@ -303,8 +308,6 @@ public:
 	virtual void draw(Int pixelX, Int pixelY, Int width, Int height) { }
 	virtual void clearShroud() { }
 	virtual void setShroudLevel(Int x, Int y, CellShroudStatus setting) { }
-	virtual void onLocalRadarObjectAdded(const RadarObject*) { }
-	virtual void onLocalRadarObjectRemoved(const RadarObject*) { }
 };
 
 #endif  // __RADAR_H_

GeneralsMD/Code/GameEngine/Source/Common/System/Radar.cpp
@@ -71,8 +71,6 @@ void Radar::deleteListResources( void )
 	while( m_localObjectList )
 	{
 
-		onLocalRadarObjectRemoved( m_localObjectList );
-
 		// get next object
 		nextObject = m_localObjectList->friend_getNext();
 
@@ -138,8 +136,15 @@ RadarObject::~RadarObject( void )
 //-------------------------------------------------------------------------------------------------
 Bool RadarObject::isTemporarilyHidden() const
 {
-	Drawable* draw = m_object->getDrawable();
-	if (draw->getStealthLook() == STEALTHLOOK_INVISIBLE || draw->isDrawableEffectivelyHidden())
+	return isTemporarilyHidden(m_object);
+}
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+Bool RadarObject::isTemporarilyHidden(const Object* obj)
+{
+	Drawable* draw = obj->getDrawable();
+	if (draw == NULL || draw->getStealthLook() == STEALTHLOOK_INVISIBLE || draw->isDrawableEffectivelyHidden())
 		return true;
 
 	return false;
@@ -394,13 +399,13 @@ void Radar::newMap( TerrainLogic *terrain )
 /** Add an object to the radar list.  The object will be sorted in the list to be grouped
 	* using it's radar priority */
 //-------------------------------------------------------------------------------------------------
-bool Radar::addObject( Object *obj )
+RadarObjectType Radar::addObject( Object *obj )
 {
 
 	// get the radar priority for this object
 	RadarPriorityType newPriority = obj->getRadarPriority();
 	if( isPriorityVisible( newPriority ) == FALSE )
-		return false;
+		return RadarObjectType_None;
 
 	// if this object is on the radar, remove it in favor of the new add
 	RadarObject **list;
@@ -466,14 +471,21 @@ bool Radar::addObject( Object *obj )
 	// set a chunk of radar data in the object
 	obj->friend_setRadarData( newObj );
 
+	RadarObjectType objectType;
 	//
 	// we will put this on either the local object list for objects that belong to the
 	// local player, or on the regular object list for all other objects
 	//
 	if( obj->isLocallyControlled() )
+	{
 		list = &m_localObjectList;
+		objectType = RadarObjectType_Local;
+	}
 	else
+	{
 		list = &m_objectList;
+		objectType = RadarObjectType_Regular;
+	}
 
 	// link object to master list at the head of it's priority section
 	if( *list == NULL )
@@ -544,12 +556,7 @@ bool Radar::addObject( Object *obj )
 
 	}  // end else
 
-	if (list == &m_localObjectList)
-	{
-		onLocalRadarObjectAdded(newObj);
-	}
-
-	return true;
+	return objectType;
 }  // end addObject
 
 //-------------------------------------------------------------------------------------------------
@@ -565,10 +572,6 @@ Bool Radar::deleteFromList( Object *obj, RadarObject **list )
 		
 		if( radarObject->friend_getObject() == obj )
 		{
-			if (list == &m_localObjectList)
-			{
-				onLocalRadarObjectRemoved( radarObject );
-			}
 
 			// unlink the object from list
 			if( prevObject == NULL )
@@ -600,24 +603,24 @@ Bool Radar::deleteFromList( Object *obj, RadarObject **list )
 //-------------------------------------------------------------------------------------------------
 /** Remove an object from the radar, the object may reside in any list */
 //-------------------------------------------------------------------------------------------------
-bool Radar::removeObject( Object *obj )
+RadarObjectType Radar::removeObject( Object *obj )
 {
 
 	// sanity
 	if( obj->friend_getRadarData() == NULL )
-		return false;
+		return RadarObjectType_None;
 
 	if( deleteFromList( obj, &m_localObjectList ) == TRUE )
-		return true;
+		return RadarObjectType_Local;
 	else if( deleteFromList( obj, &m_objectList ) == TRUE )
-		return true;
+		return RadarObjectType_Regular;
 	else
 	{
 
 		// sanity
 		DEBUG_ASSERTCRASH( 0, ("Radar: Tried to remove object '%s' which was not found",
 											 obj->getTemplate()->getName().str()) );
-		return false;
+		return RadarObjectType_None;
 	}  // end else
 
 }  // end removeObject
@@ -1558,7 +1561,7 @@ void Radar::loadPostProcess( void )
 // ------------------------------------------------------------------------------------------------
 /** Is the priority type passed in a "visible" one that can show up on the radar */
 // ------------------------------------------------------------------------------------------------
-Bool Radar::isPriorityVisible( RadarPriorityType priority ) const 
+Bool Radar::isPriorityVisible( RadarPriorityType priority )
 {
 
 	switch( priority )

GeneralsMD/Code/GameEngineDevice/Include/W3DDevice/Common/W3DRadar.h
@@ -61,9 +61,12 @@ public:
 	virtual void update( void );																	///< subsystem update
 	virtual void reset( void );																		///< subsystem reset
 
+	virtual RadarObjectType addObject( Object *obj ); ///< add object to radar
+	virtual RadarObjectType removeObject( Object *obj ); ///< remove object from radar
+
 	virtual void newMap( TerrainLogic *terrain );				///< reset radar for new map
 
-	void draw( Int pixelX, Int pixelY, Int width, Int height );		///< draw the radar
+	virtual void draw( Int pixelX, Int pixelY, Int width, Int height );		///< draw the radar
 
 	virtual void clearShroud();
 	virtual void setShroudLevel(Int x, Int y, CellShroudStatus setting);
@@ -71,11 +74,6 @@ public:
 	virtual void refreshTerrain( TerrainLogic *terrain );
 
 protected:
-
-	virtual void onLocalRadarObjectAdded( const RadarObject* radarObject );
-	virtual void onLocalRadarObjectRemoved( const RadarObject* radarObject );
-
-	void rebuildCachedHeroObjectList();
 
 	void drawSingleBeaconEvent( Int pixelX, Int pixelY, Int width, Int height, Int index );
 	void drawSingleGenericEvent( Int pixelX, Int pixelY, Int width, Int height, Int index );
@@ -87,7 +85,7 @@ protected:
 	void drawViewBox( Int pixelX, Int pixelY, Int width, Int height );  ///< draw view box
 	void buildTerrainTexture( TerrainLogic *terrain );	 ///< create the terrain texture of the radar
 	void drawIcons( Int pixelX, Int pixelY, Int width, Int height );	///< draw all of the radar icons
-	void renderObjectList( const RadarObject *listHead, TextureClass *texture );			 ///< render an object list to the texture
+	void renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero = FALSE );			 ///< render an object list to the texture
 	void interpolateColorForHeight( RGBColor *color, 
 																	Real height, 
 																	Real hiZ, 

GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/Common/System/W3DRadar.cpp
@@ -612,7 +612,7 @@ void W3DRadar::drawIcons( Int pixelX, Int pixelY, Int width, Int height )
 //-------------------------------------------------------------------------------------------------
 /** Render an object list into the texture passed in */
 //-------------------------------------------------------------------------------------------------
-void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture )
+void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *texture, Bool calcHero )
 {
 
 	// sanity
@@ -630,6 +630,12 @@ void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *text
 	if (player)
 		playerIndex=player->getPlayerIndex();
 
+	if( calcHero )
+	{
+		// clear all entries from the cached hero object list
+		m_cachedHeroObjectList.clear();
+	}
+
 	for( const RadarObject *rObj = listHead; rObj; rObj = rObj->friend_getNext() )
 	{
 
@@ -639,6 +645,11 @@ void W3DRadar::renderObjectList( const RadarObject *listHead, TextureClass *text
 		// get object
 		const Object *obj = rObj->friend_getObject();
 
+		// cache hero objects for drawing in icon layer
+		if( calcHero && obj->isHero() )
+		{
+			m_cachedHeroObjectList.push_back(obj);
+		}
     Bool skip = FALSE;
 
 		// check for shrouded status
@@ -850,11 +861,6 @@ W3DRadar::~W3DRadar( void )
 void W3DRadar::xfer( Xfer *xfer )
 {
 	Radar::xfer(xfer);
-
-	if (xfer->getXferMode() == XFER_LOAD)
-	{
-		rebuildCachedHeroObjectList();
-	}
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -957,6 +963,8 @@ void W3DRadar::reset( void )
 	// extending functionality, call base class
 	Radar::reset();
 
+	m_cachedHeroObjectList.clear();
+
 	// clear our texture data, but do not delete the resources
 	SurfaceClass *surface;
 
@@ -990,6 +998,37 @@ void W3DRadar::update( void )
 	Radar::update();
 
 }  // end update
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+RadarObjectType W3DRadar::addObject( Object* obj )
+{
+	RadarObjectType addedType = Radar::addObject(obj);
+
+	if (addedType == RadarObjectType_Local)
+	{
+		if (obj->isHero() && !RadarObject::isTemporarilyHidden(obj))
+		{
+			m_cachedHeroObjectList.push_back(obj);
+		}
+	}
+
+	return addedType;
+}
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+RadarObjectType W3DRadar::removeObject( Object* obj )
+{
+	RadarObjectType removedType = Radar::removeObject(obj);
+
+	if (removedType == RadarObjectType_Local)
+	{
+		stl::find_and_erase_unordered(m_cachedHeroObjectList, obj);
+	}
+
+	return removedType;
+}
 
 //-------------------------------------------------------------------------------------------------
 /** Reset the radar for the new map data being given to it */
@@ -1400,7 +1439,7 @@ void W3DRadar::draw( Int pixelX, Int pixelY, Int width, Int height )
 
 		// rebuild the object overlay
 		renderObjectList( getObjectList(), m_overlayTexture );
-		renderObjectList( getLocalObjectList(), m_overlayTexture );
+		renderObjectList( getLocalObjectList(), m_overlayTexture, TRUE );
 		
 	}  // end if
 
@@ -1450,41 +1489,6 @@ void W3DRadar::refreshTerrain( TerrainLogic *terrain )
 
 }  // end refreshTerrain
 
-//-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
-void W3DRadar::onLocalRadarObjectAdded( const RadarObject* radarObject )
-{
-	const Object* obj = radarObject->friend_getObject();
-	if (obj->isHero())
-	{
-		m_cachedHeroObjectList.push_back(obj);
-	}
-}
-
-//-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
-void W3DRadar::onLocalRadarObjectRemoved( const RadarObject* radarObject )
-{
-	const Object* obj = radarObject->friend_getObject();
-	if (obj->isHero())
-	{
-		stl::find_and_erase_unordered(m_cachedHeroObjectList, obj);
-	}
-}
-
-//-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
-void W3DRadar::rebuildCachedHeroObjectList()
-{
-	m_cachedHeroObjectList.clear();
-	const RadarObject* radarObject = getLocalObjectList();
-
-	while (radarObject != NULL)
-	{
-		onLocalRadarObjectAdded(radarObject);
-		radarObject = radarObject->friend_getNext();
-	}
-}
 
 
 