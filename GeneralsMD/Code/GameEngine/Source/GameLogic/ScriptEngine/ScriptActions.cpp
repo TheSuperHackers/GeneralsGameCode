22e6c6b35022ff5cbaa0b4cb035b882051a9c50b
Mauller <26652186+Mauller@users.noreply.github.com>
2025-07-17 21:37:39 +0100

    fix(view): Adjust default camera height based on aspect ratio

--- GeneralsMD/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions.cpp
@@ -4607,9 +4607,11 @@ void ScriptActions::doCameraStopTetherNamed(void)
 //-------------------------------------------------------------------------------------------------
 /** doCameraSetDefault */
 //-------------------------------------------------------------------------------------------------
-void ScriptActions::doCameraSetDefault(Real pitch, Real angle, Real maxHeight)
+void ScriptActions::doCameraSetDefault(Real pitch, Real angle, Real heighScale)
 {
-	TheTacticalView->setDefaultView(pitch, angle, maxHeight);
+	TheTacticalView->setCameraHeightAboveGroundLimitsToDefault(heighScale);
+	TheTacticalView->setPitch(pitch);
+	TheTacticalView->setAngle(angle);
 }
 
 //-------------------------------------------------------------------------------------------------
