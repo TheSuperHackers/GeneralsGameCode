b738e026a0d597995db21bf2b8150ba4094b3163
ryan-de-boer <ryan.l.de.boer@gmail.com>
2025-08-01 17:45:59 +0800

    Unify to beginning of dx8wrapper.cpp

--- Core/GameEngine/Source/Common/Graphics/DX8/dx8wrapper_common.h
+++ Core/GameEngine/Source/Common/Graphics/DX8/dx8wrapper_common.h
@@ -1,3 +1,274 @@
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/***********************************************************************************************
+ ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
+ ***********************************************************************************************
+ *                                                                                             *
+ *                 Project Name : WW3D                                                         *
+ *                                                                                             *
+ *                     $Archive:: /Commando/Code/ww3d2/dx8wrapper.cpp                         $*
+ *                                                                                             *
+ *              Original Author:: Jani Penttinen                                               *
+ *                                                                                             *
+ *                      $Author:: Kenny Mitchell                                               *
+ *                                                                                             *
+ *                     $Modtime:: 08/05/02 1:27p                                              $*
+ *                                                                                             *
+ *                    $Revision:: 170                                                         $*
+ *                                                                                             *
+ * 06/26/02 KM Matrix name change to avoid MAX conflicts                                       *
+ * 06/27/02 KM Render to shadow buffer texture support														*
+ * 06/27/02 KM Shader system updates																				*
+ * 08/05/02 KM Texture class redesign
+ *---------------------------------------------------------------------------------------------*
+ * Functions:                                                                                  *
+ *   DX8Wrapper::_Update_Texture -- Copies a texture from system memory to video memory        *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+ //#define CREATE_DX8_MULTI_THREADED
+ //#define CREATE_DX8_FPU_PRESERVE
+#define WW3D_DEVTYPE D3DDEVTYPE_HAL
+
+#if defined(_MSC_VER) && _MSC_VER < 1300
+#undef WINVER
+#define WINVER 0x0500 // Required to access GetMonitorInfo in VC6.
+#endif
+
+#include "dx8wrapper.h"
+#include "dx8webbrowser.h"
+#include "dx8fvf.h"
+#include "dx8vertexbuffer.h"
+#include "dx8indexbuffer.h"
+#include "dx8renderer.h"
+#include "ww3d.h"
+#include "camera.h"
+#include "wwstring.h"
+#include "matrix4.h"
+#include "vertmaterial.h"
+#include "rddesc.h"
+#include "lightenvironment.h"
+#include "statistics.h"
+#include "registry.h"
+#include "boxrobj.h"
+#include "pointgr.h"
+#include "render2d.h"
+#include "sortingrenderer.h"
+#include "shattersystem.h"
+#include "light.h"
+#include "assetmgr.h"
+#include "textureloader.h"
+#include "missingtexture.h"
+#include "thread.h"
+#include <stdio.h>
+#include <d3dx8core.h>
+#include "pot.h"
+#include "wwprofile.h"
+#include "ffactory.h"
+#include "dx8caps.h"
+#include "formconv.h"
+#include "dx8texman.h"
+#include "bound.h"
+#include "dx8webbrowser.h"
+
+#include "shdlib.h"
+
+const int DEFAULT_RESOLUTION_WIDTH = 640;
+const int DEFAULT_RESOLUTION_HEIGHT = 480;
+const int DEFAULT_BIT_DEPTH = 32;
+const int DEFAULT_TEXTURE_BIT_DEPTH = 16;
+
+bool DX8Wrapper_IsWindowed = true;
+
+// FPU_PRESERVE
+int DX8Wrapper_PreserveFPU = 0;
+
+/***********************************************************************************
+**
+** DX8Wrapper Static Variables
+**
+***********************************************************************************/
+
+static HWND						_Hwnd = NULL;
+bool								DX8Wrapper::IsInitted = false;
+bool								DX8Wrapper::_EnableTriangleDraw = true;
+
+int								DX8Wrapper::CurRenderDevice = -1;
+int								DX8Wrapper::ResolutionWidth = DEFAULT_RESOLUTION_WIDTH;
+int								DX8Wrapper::ResolutionHeight = DEFAULT_RESOLUTION_HEIGHT;
+int								DX8Wrapper::BitDepth = DEFAULT_BIT_DEPTH;
+int								DX8Wrapper::TextureBitDepth = DEFAULT_TEXTURE_BIT_DEPTH;
+bool								DX8Wrapper::IsWindowed = false;
+D3DFORMAT					DX8Wrapper::DisplayFormat = D3DFMT_UNKNOWN;
+
+D3DMATRIX						DX8Wrapper::old_world;
+D3DMATRIX						DX8Wrapper::old_view;
+D3DMATRIX						DX8Wrapper::old_prj;
+
+// shader system additions KJM v
+DWORD								DX8Wrapper::Vertex_Shader = 0;
+DWORD								DX8Wrapper::Pixel_Shader = 0;
+
+Vector4							DX8Wrapper::Vertex_Shader_Constants[MAX_VERTEX_SHADER_CONSTANTS];
+Vector4							DX8Wrapper::Pixel_Shader_Constants[MAX_PIXEL_SHADER_CONSTANTS];
+
+LightEnvironmentClass* DX8Wrapper::Light_Environment = NULL;
+RenderInfoClass* DX8Wrapper::Render_Info = NULL;
+
+DWORD								DX8Wrapper::Vertex_Processing_Behavior = 0;
+ZTextureClass* DX8Wrapper::Shadow_Map[MAX_SHADOW_MAPS];
+
+Vector3							DX8Wrapper::Ambient_Color;
+// shader system additions KJM ^
+
+bool								DX8Wrapper::world_identity;
+unsigned							DX8Wrapper::RenderStates[256];
+unsigned							DX8Wrapper::TextureStageStates[MAX_TEXTURE_STAGES][32];
+IDirect3DBaseTexture8* DX8Wrapper::Textures[MAX_TEXTURE_STAGES];
+RenderStateStruct				DX8Wrapper::render_state;
+unsigned							DX8Wrapper::render_state_changed;
+
+bool								DX8Wrapper::FogEnable = false;
+D3DCOLOR							DX8Wrapper::FogColor = 0;
+
+IDirect3D8* DX8Wrapper::D3DInterface = NULL;
+IDirect3DDevice8* DX8Wrapper::D3DDevice = NULL;
+IDirect3DSurface8* DX8Wrapper::CurrentRenderTarget = NULL;
+IDirect3DSurface8* DX8Wrapper::CurrentDepthBuffer = NULL;
+IDirect3DSurface8* DX8Wrapper::DefaultRenderTarget = NULL;
+IDirect3DSurface8* DX8Wrapper::DefaultDepthBuffer = NULL;
+bool								DX8Wrapper::IsRenderToTexture = false;
+
+unsigned							DX8Wrapper::matrix_changes = 0;
+unsigned							DX8Wrapper::material_changes = 0;
+unsigned							DX8Wrapper::vertex_buffer_changes = 0;
+unsigned							DX8Wrapper::index_buffer_changes = 0;
+unsigned							DX8Wrapper::light_changes = 0;
+unsigned							DX8Wrapper::texture_changes = 0;
+unsigned							DX8Wrapper::render_state_changes = 0;
+unsigned							DX8Wrapper::texture_stage_state_changes = 0;
+unsigned							DX8Wrapper::draw_calls = 0;
+unsigned							DX8Wrapper::_MainThreadID = 0;
+bool								DX8Wrapper::CurrentDX8LightEnables[4];
+bool								DX8Wrapper::IsDeviceLost;
+int								DX8Wrapper::ZBias;
+float								DX8Wrapper::ZNear;
+float								DX8Wrapper::ZFar;
+Matrix4x4						DX8Wrapper::ProjectionMatrix;
+Matrix4x4						DX8Wrapper::DX8Transforms[D3DTS_WORLD + 1];
+
+DX8Caps* DX8Wrapper::CurrentCaps = 0;
+
+// Hack test... this disables rendering of batches of too few polygons.
+unsigned							DX8Wrapper::DrawPolygonLowBoundLimit = 0;
+
+D3DADAPTER_IDENTIFIER8		DX8Wrapper::CurrentAdapterIdentifier;
+
+unsigned long DX8Wrapper::FrameCount = 0;
+
+bool								_DX8SingleThreaded = false;
+
+unsigned							number_of_DX8_calls = 0;
+static unsigned				last_frame_matrix_changes = 0;
+static unsigned				last_frame_material_changes = 0;
+static unsigned				last_frame_vertex_buffer_changes = 0;
+static unsigned				last_frame_index_buffer_changes = 0;
+static unsigned				last_frame_light_changes = 0;
+static unsigned				last_frame_texture_changes = 0;
+static unsigned				last_frame_render_state_changes = 0;
+static unsigned				last_frame_texture_stage_state_changes = 0;
+static unsigned				last_frame_number_of_DX8_calls = 0;
+static unsigned				last_frame_draw_calls = 0;
+
+static D3DDISPLAYMODE DesktopMode;
+
+static D3DPRESENT_PARAMETERS								_PresentParameters;
+static DynamicVectorClass<StringClass>					_RenderDeviceNameTable;
+static DynamicVectorClass<StringClass>					_RenderDeviceShortNameTable;
+static DynamicVectorClass<RenderDeviceDescClass>	_RenderDeviceDescriptionTable;
+
+
+typedef IDirect3D8* (WINAPI* Direct3DCreate8Type) (UINT SDKVersion);
+Direct3DCreate8Type	Direct3DCreate8Ptr = NULL;
+HINSTANCE D3D8Lib = NULL;
+
+DX8_CleanupHook* DX8Wrapper::m_pCleanupHook = NULL;
+#ifdef EXTENDED_STATS
+DX8_Stats	 DX8Wrapper::stats;
+#endif
+/***********************************************************************************
+**
+** DX8Wrapper Implementation
+**
+***********************************************************************************/
+
+void Log_DX8_ErrorCode(unsigned res)
+{
+	char tmp[256] = "";
+
+	HRESULT new_res = D3DXGetErrorStringA(
+		res,
+		tmp,
+		sizeof(tmp));
+
+	if (new_res == D3D_OK) {
+		WWDEBUG_SAY((tmp));
+	}
+
+	WWASSERT(0);
+}
+
+void Non_Fatal_Log_DX8_ErrorCode(unsigned res, const char* file, int line)
+{
+	char tmp[256] = "";
+
+	HRESULT new_res = D3DXGetErrorStringA(
+		res,
+		tmp,
+		sizeof(tmp));
+
+	if (new_res == D3D_OK) {
+		WWDEBUG_SAY(("DX8 Error: %s, File: %s, Line: %d", tmp, file, line));
+	}
+}
+
+// TheSuperHackers @info helmutbuhler 14/04/2025
+// Helper function that moves x and y such that the inner rect fits into the outer rect.
+// If the inner rect already is in the outer rect, then this does nothing.
+// If the inner rect is larger than the outer rect, then the inner rect will be aligned to the top left of the outer rect.
+void MoveRectIntoOtherRect(const RECT& inner, const RECT& outer, int* x, int* y)
+{
+	int dx = 0;
+	if (inner.right > outer.right)
+		dx = outer.right - inner.right;
+	if (inner.left < outer.left)
+		dx = outer.left - inner.left;
+
+	int dy = 0;
+	if (inner.bottom > outer.bottom)
+		dy = outer.bottom - inner.bottom;
+	if (inner.top < outer.top)
+		dy = outer.top - inner.top;
+
+	*x += dx;
+	*y += dy;
+}
+
 bool DX8Wrapper::Init(void* hwnd, bool lite)
 {
 	WWASSERT(!IsInitted);
