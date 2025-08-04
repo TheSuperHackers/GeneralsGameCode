8d4a882e7473721328cf907939cbe4b9dc977417
L3-M <100478094+L3-M@users.noreply.github.com>
2025-08-04 20:35:15 +0300

    Implement suggestion

--- Generals/Code/GameEngine/Source/GameClient/MessageStream/CommandXlat.cpp
+++ Generals/Code/GameEngine/Source/GameClient/MessageStream/CommandXlat.cpp
@@ -2737,8 +2737,10 @@ GameMessageDisposition CommandTranslator::translateGameMessage(const GameMessage
 		//-----------------------------------------------------------------------------------------
 		case GameMessage::MSG_META_SELECT_NEXT_IDLE_WORKER:
 			if (TheInGameUI)
+			{
 				TheInGameUI->selectNextIdleWorker();
-			disp = DESTROY_MESSAGE;
+				disp = DESTROY_MESSAGE;
+			}
 			break;
 
 		//-----------------------------------------------------------------------------------------
