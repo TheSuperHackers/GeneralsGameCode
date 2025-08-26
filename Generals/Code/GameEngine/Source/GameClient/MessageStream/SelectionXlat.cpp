557f81eed668fbec3d26689f634b17072cdf88d1
Stubbjax <stubbjax02@hotmail.com>
2025-08-26 22:11:03 +1000

    refactor: Consolidate click conditions

--- Generals/Code/GameEngine/Source/GameClient/MessageStream/SelectionXlat.cpp
+++ Generals/Code/GameEngine/Source/GameClient/MessageStream/SelectionXlat.cpp
@@ -910,7 +910,7 @@ GameMessageDisposition SelectionTranslator::translateGameMessage(const GameMessa
 		//-----------------------------------------------------------------------------
 		case GameMessage::MSG_RAW_MOUSE_RIGHT_BUTTON_UP:
 		{
-			ICoord2D delta, pixel;
+			ICoord2D pixel;
 			UnsignedInt currentTime;
 			Coord3D cameraPos;
 
@@ -920,20 +920,7 @@ GameMessageDisposition SelectionTranslator::translateGameMessage(const GameMessa
 			pixel = msg->getArgument( 0 )->pixel;
 			currentTime = (UnsignedInt) msg->getArgument( 2 )->integer;
 
-			delta.x = m_deselectFeedbackAnchor.x - pixel.x;
-			delta.y = m_deselectFeedbackAnchor.y - pixel.y;
-
-			Bool isClick = TRUE;
-			if (abs(delta.x) > TheMouse->m_dragTolerance || abs(delta.y) > TheMouse->m_dragTolerance)
-			{
-				isClick = FALSE;
-			}
-
-			if (isClick &&
-					currentTime - m_lastClick > TheMouse->m_dragToleranceMS)
-			{
-				isClick = FALSE;
-			}
+			Bool isClick = TheMouse->isClick(&m_deselectFeedbackAnchor, &pixel, m_lastClick, currentTime);
 
 			if (isClick &&
 					cameraPos.length() > TheMouse->m_dragTolerance3D)
