Mauller/CnC_Generals_Zero_Hour

bugfix(gui): fix the ability to enable the scroll anchor in the game options.ini

GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
@@ -57,6 +57,13 @@ CONSTEXPR const Int MAX_GLOBAL_LIGHTS = 3;
 CONSTEXPR const Int SIMULATE_REPLAYS_SEQUENTIAL = -1;
 
 //-------------------------------------------------------------------------------------------------
+enum TriState  {
+	UNINITIALIZED = -1,
+	DISABLED = 0,
+	ENABLED = 1
+};
+
+//-------------------------------------------------------------------------------------------------
 class CommandLineData
 {
 	friend class CommandLine;
@@ -372,6 +379,8 @@ public:
 
 	Real m_keyboardScrollFactor;			///< Factor applied to game scrolling speed via keyboard scrolling
 	Real m_keyboardDefaultScrollFactor;			///< Factor applied to game scrolling speed via keyboard scrolling
+	TriState m_drawScrollAnchor;					///< Set that the scroll anchor should be enabled
+	TriState m_moveScrollAnchor;					///< set that the scroll anchor should move
 
 	Bool m_animateWindows;						///< Should we animate window transitions?
 

GeneralsMD/Code/GameEngine/Include/Common/UserPreferences.h
@@ -40,6 +40,7 @@
 
 class Money;
 enum CursorCaptureMode CPP_11(: Int);
+enum TriState CPP_11(: Int);
 
 //-----------------------------------------------------------------------------
 // PUBLIC TYPES ///////////////////////////////////////////////////////////////
@@ -95,6 +96,8 @@ public:
 	Bool getRetaliationModeEnabled();					// convenience function
 	Bool getDoubleClickAttackMoveEnabled(void);	// convenience function
 	Real getScrollFactor(void);								// convenience function
+	TriState getDrawScrollAnchor(void);
+	TriState getMoveScrollAnchor(void);
 	CursorCaptureMode getCursorCaptureMode() const;
 	Bool getSendDelay(void);									// convenience function
 	Int getFirewallBehavior(void);						// convenience function

GeneralsMD/Code/GameEngine/Source/Common/GlobalData.cpp
@@ -995,6 +995,9 @@ GlobalData::GlobalData()
 	m_cameraAdjustSpeed = 0.1f;
 	m_enforceMaxCameraHeight = TRUE;
 
+	m_drawScrollAnchor = UNINITIALIZED;
+	m_moveScrollAnchor = UNINITIALIZED;
+
 	m_animateWindows = TRUE;
 
 	m_iniCRC = 0;
@@ -1192,6 +1195,8 @@ void GlobalData::parseGameDataDefinition( INI* ini )
 	TheWritableGlobalData->m_clientRetaliationModeEnabled = optionPref.getRetaliationModeEnabled();
 	TheWritableGlobalData->m_doubleClickAttackMove = optionPref.getDoubleClickAttackMoveEnabled();
 	TheWritableGlobalData->m_keyboardScrollFactor = optionPref.getScrollFactor();
+	TheWritableGlobalData->m_drawScrollAnchor = optionPref.getDrawScrollAnchor();
+	TheWritableGlobalData->m_moveScrollAnchor = optionPref.getMoveScrollAnchor();
 	TheWritableGlobalData->m_defaultIP = optionPref.getLANIPAddress();
 	TheWritableGlobalData->m_firewallSendDelay = optionPref.getSendDelay();
 	TheWritableGlobalData->m_firewallBehavior = optionPref.getFirewallBehavior();

TheSuperHackers/GeneralsGameCode

GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
@@ -372,6 +372,8 @@ public:
 
 	Real m_keyboardScrollFactor;			///< Factor applied to game scrolling speed via keyboard scrolling
 	Real m_keyboardDefaultScrollFactor;			///< Factor applied to game scrolling speed via keyboard scrolling
+	Bool m_drawScrollAnchor;					///< Set that the scroll anchor should be enabled
+	Bool m_moveScrollAnchor;					///< set that the scroll anchor should move
 
 	Bool m_animateWindows;						///< Should we animate window transitions?
 

GeneralsMD/Code/GameEngine/Include/Common/UserPreferences.h
@@ -95,6 +95,8 @@ public:
 	Bool getRetaliationModeEnabled();					// convenience function
 	Bool getDoubleClickAttackMoveEnabled(void);	// convenience function
 	Real getScrollFactor(void);								// convenience function
+	Bool getDrawScrollAnchor(void);
+	Bool getMoveScrollAnchor(void);
 	CursorCaptureMode getCursorCaptureMode() const;
 	Bool getSendDelay(void);									// convenience function
 	Int getFirewallBehavior(void);						// convenience function

GeneralsMD/Code/GameEngine/Source/Common/GlobalData.cpp
@@ -991,6 +991,8 @@ GlobalData::GlobalData()
 	m_loadScreenRender = FALSE;
 
 	m_keyboardDefaultScrollFactor = m_keyboardScrollFactor = 0.5f;
+	m_drawScrollAnchor = FALSE;
+	m_moveScrollAnchor = FALSE;
 	m_scrollAmountCutoff = 10.0f;
 	m_cameraAdjustSpeed = 0.1f;
 	m_enforceMaxCameraHeight = TRUE;
@@ -1192,6 +1194,8 @@ void GlobalData::parseGameDataDefinition( INI* ini )
 	TheWritableGlobalData->m_clientRetaliationModeEnabled = optionPref.getRetaliationModeEnabled();
 	TheWritableGlobalData->m_doubleClickAttackMove = optionPref.getDoubleClickAttackMoveEnabled();
 	TheWritableGlobalData->m_keyboardScrollFactor = optionPref.getScrollFactor();
+	TheWritableGlobalData->m_drawScrollAnchor = optionPref.getDrawScrollAnchor();
+	TheWritableGlobalData->m_moveScrollAnchor = optionPref.getMoveScrollAnchor();
 	TheWritableGlobalData->m_defaultIP = optionPref.getLANIPAddress();
 	TheWritableGlobalData->m_firewallSendDelay = optionPref.getSendDelay();
 	TheWritableGlobalData->m_firewallBehavior = optionPref.getFirewallBehavior();