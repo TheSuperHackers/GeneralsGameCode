835c9255ddd1f57ba17d094c8f18d4197a4a7189
Stubbjax <stubbjax02@hotmail.com>
2025-09-20 21:01:17 +1000

    docs: Add comment

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Body/UndeadBody.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Body/UndeadBody.cpp
@@ -84,6 +84,7 @@ void UndeadBody::attemptDamage( DamageInfo *damageInfo )
 #if RETAIL_COMPATIBLE_CRC
 			&& damageInfo->in.m_amount >= getHealth()
 #else
+			// TheSuperHackers @bugfix Stubbjax 20/09/2025 Battle Buses now correctly apply damage modifiers when calculating lethal damage
 			&& estimateDamage(damageInfo->in) >= getHealth()
 #endif
 			&& IsHealthDamagingDamage(damageInfo->in.m_damageType)
