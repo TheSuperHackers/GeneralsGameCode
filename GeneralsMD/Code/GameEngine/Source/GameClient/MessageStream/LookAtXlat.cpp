222f9efe0fa2c28feac831d0b975581d932a448a
xezon <4720891+xezon@users.noreply.github.com>
2025-10-01 20:04:39 +0200

    tweak(scrolling): Use smoothened frame rate to scale movement for scrolling and camera

--- GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/LookAtXlat.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/LookAtXlat.cpp
@@ -29,7 +29,6 @@
 #include "PreRTS.h"	// This must go first in EVERY cpp file int the GameEngine
 
 #include "Common/GameType.h"
-#include "Common/GameEngine.h"
 #include "Common/MessageStream.h"
 #include "Common/Player.h"
 #include "Common/PlayerList.h"
@@ -439,7 +438,7 @@ GameMessageDisposition LookAtTranslator::translateGameMessage(const GameMessage 
 			{
 
 				// TheSuperHackers @bugfix Mauller 07/06/2025 The camera scrolling is now decoupled from the render update.
-				const Real fpsRatio = (Real)BaseFps / TheGameEngine->getUpdateFps();
+				const Real fpsRatio = (Real)BaseFps / TheDisplay->getAverageFPS();
 
 				switch (m_scrollType)
 				{
