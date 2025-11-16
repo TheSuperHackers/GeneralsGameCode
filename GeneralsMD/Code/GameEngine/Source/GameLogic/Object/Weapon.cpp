a52000727a98d631f0bf1e685f17cc81f834b0c6
Stubbjax <stubbjax02@hotmail.com>
2025-11-16 15:31:02 +1100

    bugfix: Show weapon effects of stealthed objects for allies and observers

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Weapon.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Weapon.cpp
@@ -915,10 +915,7 @@ UnsignedInt WeaponTemplate::fireWeaponTemplate
 
 		Bool handled;
 
-		if(!sourceObj->isLocallyControlled()									// if user watching is not controller and
-			&&  sourceObj->testStatus(OBJECT_STATUS_STEALTHED)	// if unit is stealthed (like a Pathfinder)
-			&& !sourceObj->testStatus(OBJECT_STATUS_DETECTED)		// but not detected...
-			&& !sourceObj->testStatus(OBJECT_STATUS_DISGUISED)  // and not disguised...
+		if(!sourceObj->getDrawable()->isVisible()							// if user watching cannot see us
 			&& !sourceObj->isKindOf(KINDOF_MINE)								// and not a mine (which always do the FX, even if hidden)...
 			&& !isPlayFXWhenStealthed()													// and not a weapon marked to playwhenstealthed
 			)
