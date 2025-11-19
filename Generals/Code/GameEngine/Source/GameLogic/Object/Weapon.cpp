335666bf1691dc9feb6a48f4001c77d87585bf4e
Stubbjax <stubbjax02@hotmail.com>
2025-11-20 01:17:28 +1100

    refactor: Improve readability

--- Generals/Code/GameEngine/Source/GameLogic/Object/Weapon.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Weapon.cpp
@@ -887,7 +887,9 @@ UnsignedInt WeaponTemplate::fireWeaponTemplate
 		// TheSuperHackers @todo: Remove hardcoded KINDOF_MINE check and apply PlayFXWhenStealthed = Yes to the mine weapons instead.
 
 		Drawable* outerDrawable = sourceObj->getOuterObject()->getDrawable();
-		if ((!outerDrawable || !outerDrawable->isVisible())		// if user watching cannot see us
+		const Bool isVisible = outerDrawable && outerDrawable->isVisible();
+
+		if (!isVisible																				// if user watching cannot see us
 			&& !sourceObj->isKindOf(KINDOF_MINE)								// and not a mine (which always do the FX, even if hidden)...
 			&& !isPlayFXWhenStealthed()													// and not a weapon marked to playwhenstealthed
 			)
