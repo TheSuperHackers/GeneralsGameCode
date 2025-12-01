fe26dbf4d9a09a39bfa443f3faf555689fabe523
Caball009 <82909616+Caball009@users.noreply.github.com>
2025-12-01 21:18:35 +0100

    feat(hotkey): Add detection behavior for keys that are kept pressed down.

--- GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/HotKey.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/MessageStream/HotKey.cpp
@@ -69,44 +69,35 @@
 //-----------------------------------------------------------------------------
 GameMessageDisposition HotKeyTranslator::translateGameMessage(const GameMessage *msg)
 {
-	GameMessageDisposition disp = KEEP_MESSAGE;
-	GameMessage::Type t = msg->getType();
-
-	if ( t == GameMessage::MSG_RAW_KEY_UP)
+	switch (msg->getType())
 	{
+	case GameMessage::MSG_RAW_KEY_DOWN:
+		if ((msg->getArgument(1)->integer & KEY_STATE_AUTOREPEAT) == 0)
+			return KEEP_MESSAGE;
 
-		//char key = msg->getArgument(0)->integer;
-		Int keyState = msg->getArgument(1)->integer;
+		break;
+	case GameMessage::MSG_RAW_KEY_UP:
+		if (msg->getArgument(1)->integer & (KEY_STATE_CONTROL | KEY_STATE_SHIFT | KEY_STATE_ALT))
+			return KEEP_MESSAGE;
 
-		// for our purposes here, we don't care to distinguish between right and left keys,
-		// so just fudge a little to simplify things.
-		Int newModState = 0;
-
-		if( keyState & KEY_STATE_CONTROL )
-		{
-			newModState |= CTRL;
-		}
-
-		if( keyState & KEY_STATE_SHIFT )
-		{
-			newModState |= SHIFT;
-		}
-
-		if( keyState & KEY_STATE_ALT )
-		{
-			newModState |= ALT;
-		}
-		if(newModState != 0)
-			return disp;
-		WideChar key = TheKeyboard->getPrintableKey(msg->getArgument(0)->integer, 0);
-		UnicodeString uKey;
-		uKey.concat(key);
-		AsciiString aKey;
-		aKey.translate(uKey);
-		if(TheHotKeyManager && TheHotKeyManager->executeHotKey(aKey))
-			disp = DESTROY_MESSAGE;
+		break;
+	default:
+		return KEEP_MESSAGE;
 	}
-	return disp;
+
+	if (!TheHotKeyManager)
+		return KEEP_MESSAGE;
+
+	WideChar key = TheKeyboard->getPrintableKey(msg->getArgument(0)->integer, 0);
+	UnicodeString uKey;
+	uKey.concat(key);
+	AsciiString aKey;
+	aKey.translate(uKey);
+
+	if (TheHotKeyManager->executeHotKey(aKey))
+		return DESTROY_MESSAGE;
+
+	return KEEP_MESSAGE;
 }
 
 //-----------------------------------------------------------------------------
