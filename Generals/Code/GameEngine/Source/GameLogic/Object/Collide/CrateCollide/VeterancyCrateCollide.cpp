1c9bef0bf8c38a4f6b3343865745fa16ced8bf4f
Stubbjax <stubbjax02@hotmail.com>
2025-12-13 12:08:52 +1100

    tweak: Minor optimisation

--- Generals/Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollide.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollide.cpp
@@ -89,13 +89,13 @@ Bool VeterancyCrateCollide::isValidToExecute( const Object *other ) const
 	if (levelsToGain <= 0)
 		return false;
 
-	const ExperienceTracker *et = other->getExperienceTracker();
-	if (!et || !other->isTrainable())
+	if (!other->isTrainable())
 	{
 		//If the other unit can't gain experience, then we can't help promote it!
 		return false;
 	}
 
+	const ExperienceTracker* et = other->getExperienceTracker();
 	if (!et || !et->canGainExpForLevel(levelsToGain))
 		return false;
 
