571ba19b482ff47d0429b195ffd1c86b5f60693a
stm <14291421+stephanmeesters@users.noreply.github.com>
2025-12-19 20:33:40 +0100

    bugfix: Prevent setGamePaused from resuming audio in init() and reset()

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -402,7 +402,7 @@ void GameLogic::reset( void )
 	m_logicTimeScaleEnabledMemory = FALSE;
 	m_inputEnabledMemory = TRUE;
 	m_mouseVisibleMemory = TRUE;
-	setGamePaused(FALSE);
+	setGamePaused(FALSE, FALSE, FALSE, FALSE);
 	m_pauseFrame = 0;
 
 	setFPMode();
@@ -4160,7 +4160,7 @@ UnsignedInt GameLogic::getCRC( Int mode, AsciiString deepCRCFileName )
 void GameLogic::exitGame()
 {
 	// TheSuperHackers @fix The logic update must not be halted to process the game exit message.
-	setGamePaused(FALSE);
+	setGamePaused(FALSE, TRUE, TRUE, FALSE);
 	TheScriptEngine->forceUnfreezeTime();
 	TheScriptEngine->doUnfreezeTime();
 
@@ -4238,7 +4238,7 @@ void GameLogic::setGamePausedInFrame( UnsignedInt frame, Bool disableLogicTimeSc
 
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-void GameLogic::setGamePaused( Bool paused, Bool pauseMusic, Bool pauseInput )
+void GameLogic::setGamePaused( Bool paused, Bool pauseMusic, Bool pauseInput, Bool allowResumeAudio)
 {
 	// We need to ignore an unpause called when we are unpaused or else:
 	// Mouse is hidden for some reason (script or something)
@@ -4254,8 +4254,8 @@ void GameLogic::setGamePaused( Bool paused, Bool pauseMusic, Bool pauseInput )
 	// Set mouse the way it "was" <--- Was counting on right answer being set in Pause.
 
 	pauseGameLogic(paused);
-	pauseGameSound(paused);
-	pauseGameMusic(paused && pauseMusic);
+	pauseGameSound(paused, allowResumeAudio);
+	pauseGameMusic(paused && pauseMusic, allowResumeAudio);
 	pauseGameInput(paused && pauseInput);
 
 	updateDisplayBusyState();
@@ -4276,7 +4276,7 @@ void GameLogic::pauseGameLogic(Bool paused)
 
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-void GameLogic::pauseGameSound(Bool paused)
+void GameLogic::pauseGameSound(Bool paused, Bool allowResumeAudio)
 {
 	if(m_pauseSound == paused)
 		return;
@@ -4301,7 +4301,7 @@ void GameLogic::pauseGameSound(Bool paused)
 		}
 #endif
 	}
-	else
+	else if (allowResumeAudio)
 	{
 		TheAudio->resumeAudio((AudioAffect)(AudioAffect_All & ~AudioAffect_Music));
 
@@ -4319,7 +4319,7 @@ void GameLogic::pauseGameSound(Bool paused)
 
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-void GameLogic::pauseGameMusic(Bool paused)
+void GameLogic::pauseGameMusic(Bool paused, Bool allowResumeAudio)
 {
 	if(m_pauseMusic == paused)
 		return;
@@ -4330,7 +4330,7 @@ void GameLogic::pauseGameMusic(Bool paused)
 	{
 		TheAudio->pauseAudio(AudioAffect_Music);
 	}
-	else
+	else if (allowResumeAudio)
 	{
 		TheAudio->resumeAudio(AudioAffect_Music);
 	}
