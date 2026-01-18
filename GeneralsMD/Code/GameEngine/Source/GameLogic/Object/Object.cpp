83fd574949df77dbd64405bd0d582402a8c07ba7
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-01-18 15:57:16 +0100

    Fixed warnings - without logic change.

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Object.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Object.cpp
@@ -5917,8 +5917,13 @@ SpecialPowerModuleInterface* Object::findSpecialPowerModuleInterface( SpecialPow
 		if (!sp)
 			continue;
 
+		if (type == SPECIAL_INVALID)
+		{
+			return sp;
+		}
+
 		const SpecialPowerTemplate *spTemplate = sp->getSpecialPowerTemplate();
-		if (spTemplate && spTemplate->getSpecialPowerType() == type || type == SPECIAL_INVALID )
+		if (spTemplate && spTemplate->getSpecialPowerType() == type)
 		{
 			return sp;
 		}
