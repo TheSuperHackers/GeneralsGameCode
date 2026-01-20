05fa8f3a2a261e086f372fa4528c68cd785d5d19
Salem B. <jurassic.lizard@protonmail.com>
2026-01-20 21:21:28 +0100

    Create wrapper for ImGui Frame mgmt and integrate in WorldbuilderV

--- GeneralsMD/Code/GameEngine/Source/GameClient/GameClient.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GameClient.cpp
@@ -34,8 +34,7 @@
 // USER INCLUDES //////////////////////////////////////////////////////////////
 #ifdef RTS_HAS_IMGUI
 #include <imgui.h>
-#include <imgui_impl_win32.h>
-#include "imgui_impl_dx8.h"
+#include "ImGuiFrameManager.h"
 #include "dx8wrapper.h"
 #endif
 
@@ -521,9 +520,7 @@ void GameClient::update( void )
 {
 	USE_PERF_TIMER(GameClient_update)
 #ifdef RTS_HAS_IMGUI
-	ImGui_ImplDX8_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
+	ImGuiFrameManager::BeginFrame();
 	// Draw ImGui Demo Window
 	{
 		ImGui::ShowDemoWindow();
@@ -639,7 +636,7 @@ void GameClient::update( void )
 	if(TheGlobalData->m_playIntro || TheGlobalData->m_afterIntro)
 	{
 #ifdef RTS_HAS_IMGUI
-		ImGui::Render();  // Prepare render data
+		ImGuiFrameManager::EndFrame();
 #endif
 		// redraw all views, update the GUI
 		TheDisplay->DRAW();
@@ -751,7 +748,7 @@ void GameClient::update( void )
 	if (TheGlobalData->m_noDraw > TheGameLogic->getFrame() && TheGameLogic->getFrame() > 0)
 	{
 #ifdef RTS_HAS_IMGUI
-		ImGui::Render();
+		ImGuiFrameManager::EndFrame();
 #endif
 		return;
 	}
@@ -778,7 +775,7 @@ void GameClient::update( void )
 
 
 #ifdef RTS_HAS_IMGUI
-	ImGui::Render();  // Prepare render data
+	ImGuiFrameManager::EndFrame();
 #endif
 
 	{
