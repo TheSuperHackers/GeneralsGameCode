8a11b124c47f2415d9acb5486c04e9bc9e6c2943
Salem B. <jurassic.lizard@protonmail.com>
2026-01-21 13:31:48 +0100

    Finalize ImGui integration in Gen and ZH worldbuilders

--- GeneralsMD/Code/GameEngine/Source/GameClient/GameClient.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GameClient.cpp
@@ -520,7 +520,7 @@ void GameClient::update( void )
 {
 	USE_PERF_TIMER(GameClient_update)
 #ifdef RTS_HAS_IMGUI
-	ImGuiFrameManager::BeginFrame();
+	ImGui::FrameManager::BeginFrame();
 	// Draw ImGui Demo Window
 	{
 		ImGui::ShowDemoWindow();
@@ -636,7 +636,7 @@ void GameClient::update( void )
 	if(TheGlobalData->m_playIntro || TheGlobalData->m_afterIntro)
 	{
 #ifdef RTS_HAS_IMGUI
-		ImGuiFrameManager::EndFrame();
+		ImGui::FrameManager::EndFrame();
 #endif
 		// redraw all views, update the GUI
 		TheDisplay->DRAW();
@@ -748,7 +748,7 @@ void GameClient::update( void )
 	if (TheGlobalData->m_noDraw > TheGameLogic->getFrame() && TheGameLogic->getFrame() > 0)
 	{
 #ifdef RTS_HAS_IMGUI
-		ImGuiFrameManager::EndFrame();
+		ImGui::FrameManager::EndFrame();
 #endif
 		return;
 	}
@@ -775,7 +775,7 @@ void GameClient::update( void )
 
 
 #ifdef RTS_HAS_IMGUI
-	ImGuiFrameManager::EndFrame();
+	ImGui::FrameManager::EndFrame();
 #endif
 
 	{
