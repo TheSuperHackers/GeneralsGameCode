6913185d8da8ac49c8eabf211bea284e090855c2
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-02-09 19:29:18 +0100

    Changed conditional for seed value.

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/QuitMenu.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/QuitMenu.cpp
@@ -202,7 +202,7 @@ static void restartMissionMenu()
 
 	// TheSuperHackers @bugfix Caball009 07/02/2026 Reuse the previous seed value for the new skirmish match to prevent mismatches.
 	// Campaign, challenge, and skirmish single-player scenarios all use GAME_SINGLE_PLAYER and are expected to use 0 as seed value.
-	const UnsignedInt seed = (gameMode == GAME_SKIRMISH) ? TheSkirmishGameInfo->getSeed() : 0;
+	const UnsignedInt seed = (TheSkirmishGameInfo) ? TheSkirmishGameInfo->getSeed() : 0;
 
 	//
 	// if the map name was from a save game it will have "Save/" at the front of it,
