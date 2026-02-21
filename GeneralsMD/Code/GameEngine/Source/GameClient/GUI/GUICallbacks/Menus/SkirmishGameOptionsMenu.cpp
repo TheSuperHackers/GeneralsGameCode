7ff44c26ad6d90f8946c0ded9a6d0104b3eebab8
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-02-21 23:12:47 +0100

    Changed 'InitGameLogicRandom' to 'InitRandom'.

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/SkirmishGameOptionsMenu.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/SkirmishGameOptionsMenu.cpp
@@ -441,7 +441,7 @@ void reallyDoStart( void )
 
 	if (isSkirmish)
 	{
-		InitGameLogicRandom(TheSkirmishGameInfo->getSeed());
+		InitRandom(TheSkirmishGameInfo->getSeed());
 
 		GameMessage *msg = TheMessageStream->appendMessage( GameMessage::MSG_NEW_GAME );
 		msg->appendIntegerArgument(GAME_SKIRMISH);
@@ -451,7 +451,7 @@ void reallyDoStart( void )
 	}
 	else
 	{
-		InitGameLogicRandom(0);
+		InitRandom(0);
 
 		GameMessage *msg = TheMessageStream->appendMessage( GameMessage::MSG_NEW_GAME );
 		msg->appendIntegerArgument(GAME_SINGLE_PLAYER);
