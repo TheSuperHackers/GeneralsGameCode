65d679cde0c7cee754380978ff958c329a8c5b53
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-03-03 18:58:33 +0100

    Moved GetGameAudioRandomValue.

--- Core/GameEngine/Source/Common/RandomValue.cpp
+++ Core/GameEngine/Source/Common/RandomValue.cpp
@@ -180,6 +180,29 @@ DEBUG_LOG(( "InitRandom %08lx",seed));
 //
 // Integer random value
 //
+Int GetGameAudioRandomValue( int lo, int hi, const char *file, int line )
+{
+	UnsignedInt delta = hi - lo + 1;
+	Int rval;
+
+	if (delta == 0)
+		return hi;
+
+	rval = ((Int)(randomValue(theGameAudioSeed) % delta)) + lo;
+
+/**/
+#ifdef DEBUG_RANDOM_AUDIO
+DEBUG_LOG(( "%d: GetGameAudioRandomValue = %d (%d - %d), %s line %d",
+				TheGameLogic->getFrame(), rval, lo, hi, file, line ));
+#endif
+/**/
+
+	return rval;
+}
+
+//
+// Integer random value
+//
 Int GetGameLogicRandomValue( int lo, int hi, const char *file, int line )
 {
 	//Int delta = hi - lo + 1;
@@ -261,29 +284,6 @@ Int GetGameClientRandomValue( int lo, int hi, const char *file, int line )
 #ifdef DEBUG_RANDOM_CLIENT
 DEBUG_LOG(( "%d: GetGameClientRandomValue = %d (%d - %d), %s line %d",
 				TheGameLogic ? TheGameLogic->getFrame() : -1, rval, lo, hi, file, line ));
-#endif
-/**/
-
-	return rval;
-}
-
-//
-// Integer random value
-//
-Int GetGameAudioRandomValue( int lo, int hi, const char *file, int line )
-{
-	UnsignedInt delta = hi - lo + 1;
-	Int rval;
-
-	if (delta == 0)
-		return hi;
-
-	rval = ((Int)(randomValue(theGameAudioSeed) % delta)) + lo;
-
-/**/
-#ifdef DEBUG_RANDOM_AUDIO
-DEBUG_LOG(( "%d: GetGameAudioRandomValue = %d (%d - %d), %s line %d",
-				TheGameLogic->getFrame(), rval, lo, hi, file, line ));
 #endif
 /**/
 
