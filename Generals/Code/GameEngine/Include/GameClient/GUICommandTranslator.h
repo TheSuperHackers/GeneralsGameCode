3b172b7da17ee37e4b4faf38a1f5d238d0e59202
Bobby Battista <bobtista@gmail.com>
2026-03-03 18:31:00 -0500

    refactor(Generals): Add override keyword to virtual function overrides in GameEngine

--- Generals/Code/GameEngine/Include/GameClient/GUICommandTranslator.h
+++ Generals/Code/GameEngine/Include/GameClient/GUICommandTranslator.h
@@ -42,7 +42,7 @@ class GUICommandTranslator : public GameMessageTranslator
 public:
 
 	GUICommandTranslator();
-	~GUICommandTranslator();
+	~GUICommandTranslator() override;
 
-	virtual GameMessageDisposition translateGameMessage( const GameMessage *msg );
+	virtual GameMessageDisposition translateGameMessage( const GameMessage *msg ) override;
 };
