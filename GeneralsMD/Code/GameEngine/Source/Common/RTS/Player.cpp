72575e2d8cae8217444387676fc11cfbbbf4be9b
Stubbjax <stubbjax02@hotmail.com>
2026-03-05 10:53:14 +1100

    docs: Remove obsolete comments

--- GeneralsMD/Code/GameEngine/Source/Common/RTS/Player.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/RTS/Player.cpp
@@ -3414,7 +3414,6 @@ Bool Player::doesObjectQualifyForBattlePlan( Object *obj ) const
 }
 
 //-------------------------------------------------------------------------------------------------
-// note, bonus is an in-out parm.
 void Player::changeBattlePlan( BattlePlanStatus plan, Int delta, const BattlePlanBonusesData *bonus )
 {
 	DUMPBATTLEPLANBONUSES(bonus, this, nullptr);
