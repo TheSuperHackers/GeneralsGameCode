52ab0204c6a31562463d44bbbb39c60574aa3df0
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-04-10 22:12:48 +0200

    fix(pathfinder): Fix CRC computation of PathFinder::m_wallPieces.

--- Core/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
+++ Core/GameEngine/Source/GameLogic/AI/AIPathfind.cpp
@@ -11338,10 +11338,20 @@ void Pathfinder::crc( Xfer *xfer )
 
 	xfer->xferInt(&m_numWallPieces);
 	CRCDEBUG_LOG(("m_numWallPieces: %8.8X", ((XferCRC *)xfer)->getCRC()));
-	for (Int i=0; i<MAX_WALL_PIECES; ++i)
+
+	for (Int i = 0; i < MAX_WALL_PIECES; ++i)
 	{
-		xfer->xferObjectID(&m_wallPieces[MAX_WALL_PIECES]);
+#if RETAIL_COMPATIBLE_CRC
+		// TheSuperHackers @info The original code effectively wrote m_numWallPieces 128 times,
+		// because it used &m_wallPieces[MAX_WALL_PIECES] which is out-of-bounds and points to m_numWallPieces.
+		static_assert(sizeof(Int) == sizeof(ObjectID));
+
+		xfer->xferInt(&m_numWallPieces);
+#else
+		xfer->xferObjectID(&m_wallPieces[i]);
+#endif
 	}
+
 	CRCDEBUG_LOG(("m_wallPieces: %8.8X", ((XferCRC *)xfer)->getCRC()));
 
 	xfer->xferReal(&m_wallHeight);
