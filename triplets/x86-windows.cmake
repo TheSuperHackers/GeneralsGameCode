0717bfce98ea9242e702f726588f1527f70786ff
githubawn <115191165+githubawn@users.noreply.github.com>
2026-04-20 23:06:25 +0200

    switch back to static linking

---
+++ triplets/x86-windows.cmake
@@ -0,0 +1,13 @@
+# Include the default x86-windows triplet
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+if(PORT MATCHES "sdl3")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+# Exclude compiler version from ABI hash so that weekly GitHub runner image
+# updates don't invalidate the binary cache. Minor MSVC version bumps do not
+# cause ABI incompatibilities for this project.
+set(VCPKG_DISABLE_COMPILER_TRACKING ON)
