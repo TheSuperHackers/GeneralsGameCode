90b207deee4dddbbfa70313bddc34399edb629ba
Stubbjax <stubbjax02@hotmail.com>
2026-05-05 12:43:50 +1000

    refactor: Convert fix into a standalone block

--- Generals/Code/GameEngine/Source/GameLogic/AI/AIStates.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/AI/AIStates.cpp
@@ -6272,13 +6272,14 @@ StateReturnType AIExitState::update()
 		//GS.  The goal of unified ExitInterfaces dies a horrible death.  I can't ask Object for the exit,
 		// as removeFromContain is only in the Contain type.  I'm splitting the names in shame.
 		ExitInterface* goalExitInterface = goal->getContain() ? goal->getContain()->getContainExitInterface() : nullptr;
-#if RETAIL_COMPATIBLE_CRC
 		if( goalExitInterface == nullptr )
-#else
-		// TheSuperHackers @bugfix Stubbjax 03/05/2026 Stop trying to exit if the container is dead, as we are already ejected.
-		if (goalExitInterface == nullptr || goal->isEffectivelyDead())
-#endif
 			return STATE_FAILURE;
+
+#if !RETAIL_COMPATIBLE_CRC
+		// TheSuperHackers @bugfix Stubbjax 03/05/2026 Stop trying to exit if the container is dead, as we are already ejected.
+		if (goal->isEffectivelyDead())
+			return STATE_FAILURE;
+#endif
 
 		if( goalExitInterface->isExitBusy() )
 			return STATE_CONTINUE;// Just wait a sec.
