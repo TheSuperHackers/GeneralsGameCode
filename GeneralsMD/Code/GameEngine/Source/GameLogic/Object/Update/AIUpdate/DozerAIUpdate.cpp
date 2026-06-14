122edf070c33f4ed58112b64b0b5461127234332
Stubbjax <stubbjax02@hotmail.com>
2026-06-15 02:38:12 +1000

    tweak: Still clear previous task if the resumed build fails

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/DozerAIUpdate.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/DozerAIUpdate.cpp
@@ -2092,13 +2092,12 @@ void DozerAIUpdate::resumePreviousTask()
 	if (m_previousTask == DOZER_TASK_BUILD)
 	{
 		Object* target = TheGameLogic->findObjectByID(m_previousTaskInfo.m_targetObjectID);
-		if (target && !target->testStatus(OBJECT_STATUS_UNDER_CONSTRUCTION))
-			return;
-
-		newTask(m_previousTask, TheGameLogic->findObjectByID(m_previousTaskInfo.m_targetObjectID));
-		m_previousTask = DOZER_TASK_INVALID;
-		m_previousTaskInfo = DozerTaskInfo();
+		if (!target || target->testStatus(OBJECT_STATUS_UNDER_CONSTRUCTION))
+			newTask(m_previousTask, TheGameLogic->findObjectByID(m_previousTaskInfo.m_targetObjectID));
 	}
+
+	m_previousTask = DOZER_TASK_INVALID;
+	m_previousTaskInfo = DozerTaskInfo();
 }
 
 //-------------------------------------------------------------------------------------------------
