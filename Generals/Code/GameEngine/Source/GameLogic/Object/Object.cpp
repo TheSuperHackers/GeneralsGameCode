176ff405d362f2b3119bc30f66aa89820e6c15d8
githubawn <115191165+githubawn@users.noreply.github.com>
2026-07-08 21:09:25 +0200

    disable cargo plane alarms

--- Generals/Code/GameEngine/Source/GameLogic/Object/Object.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Object.cpp
@@ -1769,7 +1769,11 @@ void Object::attemptDamage( DamageInfo *damageInfo )
 			damageInfo->in.m_damageType != DAMAGE_HEALING &&
 			!BitIsSet(damageInfo->in.m_sourcePlayerMask, getControllingPlayer()->getPlayerMask()) &&
 			m_radarData != nullptr &&
-			isLocallyControlled() )
+			isLocallyControlled()
+			#if !PRESERVE_CARGO_PLANE_ATTACK_WARNINGS
+				&& getTemplate()->getName() != "AmericaJetCargoPlane"
+			#endif
+			)
 		TheRadar->tryUnderAttackEvent( this );
 
 }
