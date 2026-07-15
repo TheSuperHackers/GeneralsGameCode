64594965288a7cd4431b03515c3d055035f2d533
githubawn <115191165+githubawn@users.noreply.github.com>
2026-07-15 19:46:36 +0200

    Replace cargo plane name check with NO_ATTACK_WARNING KindOf

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Object.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Object.cpp
@@ -1969,11 +1969,8 @@ void Object::attemptDamage( DamageInfo *damageInfo )
 			getControllingPlayer() &&
 			!BitIsSet(damageInfo->in.m_sourcePlayerMask, getControllingPlayer()->getPlayerMask()) &&
 			m_radarData != nullptr &&
-			isLocallyControlled()
-#if !PRESERVE_CARGO_PLANE_ATTACK_WARNINGS
-			&& getTemplate()->getName() != "AmericaJetCargoPlane"
-#endif
-			)
+			isLocallyControlled() &&
+			!isKindOf( KINDOF_NO_ATTACK_WARNING ) )
 		TheRadar->tryUnderAttackEvent( this );
 
 }
