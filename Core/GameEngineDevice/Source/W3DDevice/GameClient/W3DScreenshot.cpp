ced0c4f9dab4ba3c77aa6becba9246e537d64532
Bobby Battista <bobtista@gmail.com>
2026-07-17 18:38:01 +0400

    refactor(screenshot): Qualify MPSCIntrusiveQueue include with WWLib prefix

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DScreenshot.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DScreenshot.cpp
@@ -22,7 +22,7 @@
 #include "GameClient/InGameUI.h"
 #include "WW3D2/dx8wrapper.h"
 #include "WW3D2/surfaceclass.h"
-#include "mpsc_intrusive_queue.h"
+#include "WWLib/mpsc_intrusive_queue.h"
 #include <stb_image_write.h>
 
 struct ScreenshotThreadData
