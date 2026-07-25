7880e99073a208a7659960cf60f57ba9fb474d03
Bobby Battista <bobtista@gmail.com>
2026-07-25 16:42:03 +0100

    bugfix(replay): Reject playback after failed frame read

--- GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
@@ -1148,8 +1148,6 @@ Bool RecorderClass::playbackFile(AsciiString filename)
 	DEBUG_ASSERTCRASH(!exeDifferent && !iniDifferent, (debugString.str()));
 #endif
 
-	TheWritableGlobalData->m_pendingFile = m_gameInfo.getMap();
-
 #ifdef DEBUG_LOGGING
 	if (header.localPlayerIndex >= 0)
 	{
@@ -1182,6 +1180,13 @@ Bool RecorderClass::playbackFile(AsciiString filename)
 	TheCommandList->reset();
 
 	readNextFrame();
+	// readNextFrame() closes m_file via stopPlayback() if the first frame cannot be read.
+	if(m_file == nullptr)
+	{
+		return FALSE;
+	}
+
+	TheWritableGlobalData->m_pendingFile = m_gameInfo.getMap();
 
 	// send a message to the logic for a new game
 	if (!m_doingAnalysis)
