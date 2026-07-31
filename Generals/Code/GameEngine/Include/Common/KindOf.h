3cb5ecb2cbad7bd80f38bed2a8d097de935b7597
githubawn <115191165+githubawn@users.noreply.github.com>
2026-07-31 23:34:10 +0200

    update comment

--- Generals/Code/GameEngine/Include/Common/KindOf.h
+++ Generals/Code/GameEngine/Include/Common/KindOf.h
@@ -174,7 +174,7 @@ enum KindOfType CPP_11(: Int)
 
 	// TheSuperHackers @info New kinds for Mods
 
-	KINDOF_NO_ATTACK_WARNING,				///< does not trigger the under attack radar/EVA warning when damaged (e.g. cargo planes)
+	KINDOF_NO_ATTACK_WARNING,				///< does not trigger the under attack radar/EVA warning when taking damage
 
 	KINDOF_COUNT,										// total number of kindofs
 	KINDOF_FIRST = 0,
