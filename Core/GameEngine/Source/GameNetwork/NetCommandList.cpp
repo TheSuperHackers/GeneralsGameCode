a59bf6d9f11d50e5664952bfa31bbb8325a049fe
Jacob Ledbetter <jledbetter460@gmail.com>
2026-08-03 07:47:57 -0600

    refactor(network): Clarify command list fast path

--- Core/GameEngine/Source/GameNetwork/NetCommandList.cpp
+++ Core/GameEngine/Source/GameNetwork/NetCommandList.cpp
@@ -134,9 +134,20 @@ static bool isCommandIdNewer(UnsignedShort newVal, UnsignedShort oldVal)
 #endif
 }
 
+static bool isCommandOrderedAfter(const NetCommandMsg *candidate, const NetCommandMsg *reference)
+{
+	if (candidate->getNetCommandType() != reference->getNetCommandType()) {
+		return candidate->getNetCommandType() > reference->getNetCommandType();
+	}
+	if (candidate->getPlayerID() != reference->getPlayerID()) {
+		return candidate->getPlayerID() > reference->getPlayerID();
+	}
+	return isCommandIdNewer(candidate->getSortNumber(), reference->getSortNumber());
+}
+
 /**
  * Insert sorts msg.  Assumes that all the previous message inserts were done using this function.
- * The message is sorted in based first on command type, then player id, and then command id.
+ * The message is sorted based first on command type, then player id, and then sort number.
  */
 NetCommandRef * NetCommandList::addMessage(NetCommandMsg *cmdMsg) {
 	if (cmdMsg == nullptr) {
@@ -166,16 +177,22 @@ NetCommandRef * NetCommandList::addMessage(NetCommandRef *&msg) {
 		// Messages that are inserted in order should just be put in one right after the other.
 		// So saving the placement of the last message inserted can give us a huge boost in
 		// efficiency.
-		NetCommandRef *theNext = m_lastMessageInserted->getNext();
-		if ((m_lastMessageInserted->getCommand()->getNetCommandType() == msg->getCommand()->getNetCommandType()) &&
-			(m_lastMessageInserted->getCommand()->getPlayerID() == msg->getCommand()->getPlayerID()) &&
-			isCommandIdNewer(msg->getCommand()->getSortNumber(), m_lastMessageInserted->getCommand()->getSortNumber()) &&
-			((theNext == nullptr) || ((theNext->getCommand()->getNetCommandType() > msg->getCommand()->getNetCommandType()) ||
-			 (theNext->getCommand()->getPlayerID() > msg->getCommand()->getPlayerID()) ||
-			 isCommandIdNewer(theNext->getCommand()->getSortNumber(), msg->getCommand()->getSortNumber())))) {
+		NetCommandMsg *command = msg->getCommand();
+		NetCommandMsg *lastCommand = m_lastMessageInserted->getCommand();
+		NetCommandRef *nextCommandRef = m_lastMessageInserted->getNext();
+
+		bool canInsertAfterLast = lastCommand->getNetCommandType() == command->getNetCommandType()
+			&& lastCommand->getPlayerID() == command->getPlayerID()
+			&& isCommandIdNewer(command->getSortNumber(), lastCommand->getSortNumber());
+
+		if (canInsertAfterLast && nextCommandRef != nullptr) {
+			canInsertAfterLast = isCommandOrderedAfter(nextCommandRef->getCommand(), command);
+		}
+
+		if (canInsertAfterLast) {
 
 			// Make sure this command isn't already in the list.
-			if (isEqualCommandMsg(m_lastMessageInserted->getCommand(), msg->getCommand())) {
+			if (isEqualCommandMsg(lastCommand, command)) {
 
 				// This command is already in the list, don't duplicate it.
 				deleteInstance(msg);
@@ -183,20 +200,17 @@ NetCommandRef * NetCommandList::addMessage(NetCommandRef *&msg) {
 				return nullptr;
 			}
 
-			if (theNext == nullptr) {
-				// this means that m_lastMessageInserted == m_last, so m_last should point to the msg that is being inserted.
-				msg->setNext(m_lastMessageInserted->getNext());
-				msg->setPrev(m_lastMessageInserted);
-				m_lastMessageInserted->setNext(msg);
-				m_lastMessageInserted = msg;
+			msg->setNext(nextCommandRef);
+			msg->setPrev(m_lastMessageInserted);
+			m_lastMessageInserted->setNext(msg);
+
+			if (nextCommandRef == nullptr) {
 				m_last = msg;
 			} else {
-				msg->setNext(m_lastMessageInserted->getNext());
-				msg->setPrev(m_lastMessageInserted);
-				m_lastMessageInserted->setNext(msg);
-				msg->getNext()->setPrev(msg);
-				m_lastMessageInserted = msg;
+				nextCommandRef->setPrev(msg);
 			}
+
+			m_lastMessageInserted = msg;
 			return msg;
 		}
 	}
@@ -291,8 +305,7 @@ NetCommandRef * NetCommandList::addMessage(NetCommandRef *&msg) {
 		return msg;
 	}
 
-	// Find the position within the player's section based on the command ID.
-	// If the command type doesn't require a command ID, sort by whatever it should be sorted by.
+	// Find the position within the player's section based on the sort number.
 	while (tempmsg != nullptr
 		&& msg->getCommand()->getNetCommandType() == tempmsg->getCommand()->getNetCommandType()
 		&& msg->getCommand()->getPlayerID() == tempmsg->getCommand()->getPlayerID()
