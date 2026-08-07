c91bc42ffb0e418ad445abd014faa7e26f37cb9c
Jaredl-Dev <260281143+Jaredl-Dev@users.noreply.github.com>
2026-08-07 12:07:08 -0700

    unify(w3ddevice): Move W3DDevice/Common to Core (#3073)

--- scripts/cpp/unify_move_files.py
+++ scripts/cpp/unify_move_files.py
@@ -667,6 +667,15 @@ def main():
     #unify_file(Game.ZEROHOUR, "GameEngine/Source/GameClient/GUI/GUICallbacks/MessageBox.cpp", Game.CORE, "GameEngine/Source/GameClient/GUI/GUICallbacks/MessageBox.cpp")
     #unify_file(Game.ZEROHOUR, "GameEngine/Source/GameClient/GUI/GUICallbacks/ReplayControls.cpp", Game.CORE, "GameEngine/Source/GameClient/GUI/GUICallbacks/ReplayControls.cpp")
 
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Include/W3DDevice/Common/W3DFunctionLexicon.h", Game.CORE, "GameEngineDevice/Include/W3DDevice/Common/W3DFunctionLexicon.h")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Source/W3DDevice/Common/System/W3DFunctionLexicon.cpp", Game.CORE, "GameEngineDevice/Source/W3DDevice/Common/System/W3DFunctionLexicon.cpp")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Include/W3DDevice/Common/W3DModuleFactory.h", Game.CORE, "GameEngineDevice/Include/W3DDevice/Common/W3DModuleFactory.h")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Source/W3DDevice/Common/Thing/W3DModuleFactory.cpp", Game.CORE, "GameEngineDevice/Source/W3DDevice/Common/Thing/W3DModuleFactory.cpp")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Include/W3DDevice/Common/W3DThingFactory.h", Game.CORE, "GameEngineDevice/Include/W3DDevice/Common/W3DThingFactory.h")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Source/W3DDevice/Common/Thing/W3DThingFactory.cpp", Game.CORE, "GameEngineDevice/Source/W3DDevice/Common/Thing/W3DThingFactory.cpp")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Include/W3DDevice/Common/W3DConvert.h", Game.CORE, "GameEngineDevice/Include/W3DDevice/Common/W3DConvert.h")
+    #unify_file(Game.ZEROHOUR, "GameEngineDevice/Source/W3DDevice/Common/W3DConvert.cpp", Game.CORE, "GameEngineDevice/Source/W3DDevice/Common/W3DConvert.cpp")
+
     return
 
 
