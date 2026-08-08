b2a5fa06dd73e30f5a61ef3d221d5f56f45955e2
Stubbjax <stubbjax02@hotmail.com>
2026-08-09 01:59:02 +1000

    bugfix: Save helicopter landing position

--- Generals/Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/JetAIUpdate.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/JetAIUpdate.cpp
@@ -2465,7 +2465,11 @@ void JetAIUpdate::xfer( Xfer *xfer )
 {
 
   // version
-  XferVersion currentVersion = 2;
+#if RETAIL_COMPATIBLE_CRC || RETAIL_COMPATIBLE_XFER_SAVE
+	XferVersion currentVersion = 2;
+#else
+	XferVersion currentVersion = 3;
+#endif
   XferVersion version = currentVersion;
   xfer->xferVersion( &version, currentVersion );
 
@@ -2503,6 +2507,11 @@ void JetAIUpdate::xfer( Xfer *xfer )
 	if( version >= 2 )
 	{
 		xfer->xferBool( &m_enginesOn );
+
+		if (version >= 3)
+		{
+			xfer->xferCoord3D(&m_landingPosForHelipadStuff);
+		}
 	}
 	else
 	{
