f4edceff8f74c6d3ffbe7c39166d988a24402e60
xezon <4720891+xezon@users.noreply.github.com>
2026-08-14 19:55:08 +0200

    Simplify far z clamp

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DView.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DView.cpp
@@ -797,7 +797,7 @@ void W3DView::updateCameraClipPlanes(const Matrix3D &transform)
 		const Real projectedRadiusToEdge = fabs(dx * camDir.X) + fabs(dy * camDir.Y);
 
 		// Final far plane
-		farZ = projectedDistanceToCenter + projectedRadiusToEdge;
+		farZ = std::max(projectedDistanceToCenter + projectedRadiusToEdge, 0.0f);
 	}
 	else
 	{
@@ -821,10 +821,7 @@ void W3DView::updateCameraClipPlanes(const Matrix3D &transform)
 		}
 	}
 
-	if (farZ >= 0.0f)
-	{
-		m_3DCamera->Set_Clip_Planes(NearZ, farZ);
-	}
+	m_3DCamera->Set_Clip_Planes(NearZ, farZ);
 }
 
 //-------------------------------------------------------------------------------------------------
