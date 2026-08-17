81a318786a844f508273fea015d77c128aedc4e2
Mauller <26652186+Mauller@users.noreply.github.com>
2026-08-17 23:09:07 +0100

    refactor(particlesys): Change drawSmudge check to be an early continue/return to reduce nesting

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DParticleSys.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DParticleSys.cpp
@@ -159,28 +159,28 @@ void W3DParticleSystemManager::doParticles(RenderInfoClass &rinfo)
 		// Handle smudge type particles
 		if (sys->isUsingSmudge())
 		{
-			if (drawSmudge)
+			if (!drawSmudge)
+				continue;
+
+			for (Particle *p = sys->getFirstParticle(); p; p = p->m_systemNext)
 			{
-				for (Particle *p = sys->getFirstParticle(); p; p = p->m_systemNext)
+				const Coord3D *pos = p->getPosition();
+				Real psize = p->getSize();
+
+				//Cull particle to edges of screen and terrain.
+				if (WWMath::Fabs( pos->x - bcX ) > ( beX + psize ) )
+					continue;
+
+				if (WWMath::Fabs( pos->y - bcY ) > ( beY + psize ) )
+					continue;
+
+				if (WWMath::Fabs( pos->z - bcZ ) > ( beZ + psize ) )
+					continue;
+
+				if (Smudge *smudge = TheSmudgeManager->findSmudge(p))
 				{
-					const Coord3D *pos = p->getPosition();
-					Real psize = p->getSize();
-
-					//Cull particle to edges of screen and terrain.
-					if (WWMath::Fabs( pos->x - bcX ) > ( beX + psize ) )
-						continue;
-
-					if (WWMath::Fabs( pos->y - bcY ) > ( beY + psize ) )
-						continue;
-
-					if (WWMath::Fabs( pos->z - bcZ ) > ( beZ + psize ) )
-						continue;
-
-					if (Smudge *smudge = TheSmudgeManager->findSmudge(p))
-					{
-						// The particle is in view. Draw the smudge!
-						smudge->m_draw = true;
-					}
+					// The particle is in view. Draw the smudge!
+					smudge->m_draw = true;
 				}
 			}
 			continue;
