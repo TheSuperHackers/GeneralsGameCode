525bbe9d247afcf55274a355c5fbc1b8607397a8
Stubbjax <stubbjax02@hotmail.com>
2026-08-19 13:55:55 +1000

    docs: Add comments

--- Generals/Code/GameEngine/Source/GameLogic/Object/Contain/TransportContain.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Contain/TransportContain.cpp
@@ -506,6 +506,7 @@ void TransportContain::onCapture( Player *oldOwner, Player *newOwner )
 #if RETAIL_COMPATIBLE_CRC
 			orderAllPassengersToExit( CMD_FROM_AI );
 #else
+			// TheSuperHackers @bugfix Stubbjax 20/11/2025 Only eject passengers if the new owner is not allied with the old owner.
 			if (oldOwner->getRelationship(newOwner->getDefaultTeam()) != ALLIES)
 				orderAllPassengersToExit(CMD_FROM_AI);
 #endif
--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Contain/TransportContain.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Contain/TransportContain.cpp
@@ -647,6 +647,7 @@ void TransportContain::onCapture( Player *oldOwner, Player *newOwner )
 #if RETAIL_COMPATIBLE_CRC
 			orderAllPassengersToExit( CMD_FROM_AI, FALSE );
 #else
+      // TheSuperHackers @bugfix Stubbjax 20/11/2025 Only eject passengers if the new owner is not allied with the old owner.
 			if (oldOwner->getRelationship(newOwner->getDefaultTeam()) != ALLIES)
 				orderAllPassengersToExit(CMD_FROM_AI, FALSE);
 #endif
