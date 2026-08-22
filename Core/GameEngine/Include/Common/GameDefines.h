bcf2d944dce0f84f3a2313dfded20c5757040b0b
Omar Aglan <omar.aglan91@yahoo.com>
2026-08-22 22:14:37 +0300

    unify(map): Merge GameLogic map headers and implementations (#3189)

--- Core/GameEngine/Include/Common/GameDefines.h
+++ Core/GameEngine/Include/Common/GameDefines.h
@@ -20,6 +20,8 @@
 
 #include "WWLib/WWDefines.h"
 
+// Note: Retail compatibility must not be broken before this project officially does.
+// Use RETAIL_COMPATIBLE_CRC, RETAIL_COMPATIBLE_DATA and RETAIL_COMPATIBLE_XFER_SAVE to guard breaking changes.
 #ifndef PRESERVE_BUILDING_RESUMPTION_DELAY
 #define PRESERVE_BUILDING_RESUMPTION_DELAY (0) // The fix for this unfavorable behavior was approved by the Game Design Committee.
 #endif
@@ -101,6 +103,10 @@
 
 #ifndef RETAIL_COMPATIBLE_CRC
 #define RETAIL_COMPATIBLE_CRC (1) // Game is expected to be CRC compatible with retail Generals 1.08, Zero Hour 1.04
+#endif
+
+#ifndef RETAIL_COMPATIBLE_DATA
+#define RETAIL_COMPATIBLE_DATA (1) // Game and tool data files are expected to be compatible with retail Generals 1.08 and Zero Hour 1.04
 #endif
 
 #ifndef RETAIL_COMPATIBLE_XFER_SAVE
