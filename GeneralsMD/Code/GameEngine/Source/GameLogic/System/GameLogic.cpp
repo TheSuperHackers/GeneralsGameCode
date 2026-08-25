96d1c9ac5285a4866a4143951df214678e014270
Caball009 <82909616+Caball009@users.noreply.github.com>
2026-08-25 22:44:09 +0200

    Refactored slot setup code in 'GameLogic::tryStartNewGame'.

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -1283,28 +1283,18 @@ void GameLogic::tryStartNewGame( Bool loadingSaveGame )
 	Bool isSkirmishOrSkirmishReplay = FALSE;
 	if (TheGameInfo)
 	{
+		if (!loadingSaveGame && m_gameMode == GAME_SKIRMISH)
+		{
+			TheSkirmishGameInfo->handleOriginalSetups();
+		}
+
 		for (Int i=0; i<MAX_SLOTS; ++i)
 		{
-			GameSlot *slot = TheGameInfo->getSlot(i);
-			if (!loadingSaveGame) {
-				if (slot->hasSavedOriginalSetup())
-				{
-					DEBUG_ASSERTCRASH(m_gameMode == GAME_SKIRMISH, ("Expected GAME_SKIRMISH but got %s", toString(m_gameMode)));
-
-					// TheSuperHackers @fix Caball009 19/03/2026 Random color, position and faction are based on the logical seed. For improved determinism,
-					// restarted games now set the original values so that the games start with the exact same logical seed values as the first time.
-					slot->setColor(slot->getOriginalColor());
-					slot->setStartPos(slot->getOriginalStartPos());
-					slot->setPlayerTemplate(slot->getOriginalPlayerTemplate());
-				}
-				else
-				{
-					slot->saveOriginalSetup();
-				}
-			}
+			GameSlot* slot = TheGameInfo->getSlot(i);
 			if (slot->isAI())
 			{
 				isSkirmishOrSkirmishReplay = TRUE;
+				break;
 			}
 		}
 	} else {
