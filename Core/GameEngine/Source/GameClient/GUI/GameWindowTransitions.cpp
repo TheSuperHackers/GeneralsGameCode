6d55b3f6ddb8394b8d83c95b7d14a755447d7fd7
Jacob Ledbetter <jledbetter460@gmail.com>
2026-09-14 10:44:08 -0600

    fix(GaneClient): Return when g is null before comparing it with the pending and current groups

--- Core/GameEngine/Source/GameClient/GUI/GameWindowTransitions.cpp
+++ Core/GameEngine/Source/GameClient/GUI/GameWindowTransitions.cpp
@@ -533,7 +533,13 @@ void GameWindowTransitionsHandler::reverse( AsciiString groupName )
 
 void GameWindowTransitionsHandler::remove( AsciiString groupName,  Bool skipPending )
 {
-	TransitionGroup *g = findGroup(groupName);
+	TransitionGroup* g = findGroup(groupName);
+
+	if (!g)
+	{
+		return;
+	}
+
 	if(m_pendingGroup == g)
 	{
 		if(skipPending)
@@ -541,6 +547,7 @@ void GameWindowTransitionsHandler::remove( AsciiString groupName,  Bool skipPend
 
 		m_pendingGroup = nullptr;
 	}
+
 	if(m_currentGroup == g)
 	{
 		m_currentGroup->skip();
