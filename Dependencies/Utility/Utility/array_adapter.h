7bb1bb88dc92bbc45bcdf8427e79f7507d8903e7
stm <14291421+stephanmeesters@users.noreply.github.com>
2026-09-17 18:01:12 +0200

    Fix guard

--- Dependencies/Utility/Utility/array_adapter.h
+++ Dependencies/Utility/Utility/array_adapter.h
@@ -22,7 +22,7 @@
 // VC6 also rejects aggregate initialization when the element type is const.
 #pragma once
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1300
+#if !(defined(_MSC_VER) && _MSC_VER < 1300)
 
 #include <array>
 
