a8fca66e019d228a511c5f2d27e07160d0eb9cc8
Skyaero <21192585+Skyaero42@users.noreply.github.com>
2026-09-19 20:39:00 +0200

    refactor(pathfinder): Move PathfindLayer class definition to its own file (#3334)

--- Core/GameEngine/Include/GameLogic/AIPathfind.h
+++ Core/GameEngine/Include/GameLogic/AIPathfind.h
@@ -38,6 +38,7 @@
 #include "Pathfinder/PathfindCell.h"
 #include "Pathfinder/PathfindCellInfo.h"
 #include "Pathfinder/PathfindCellList.h"
+#include "Pathfinder/PathfindLayer.h"
 #include "Pathfinder/PathNode.h"
 
 class Bridge;
@@ -70,61 +71,6 @@ enum {MAX_WALL_PIECES = 128};
 
 // how close a unit has to be in z to interact with the layer.
 #define LAYER_Z_CLOSE_ENOUGH_F 10.0f
-/**
- * This class represents a bridge in the map. This is effectively
- * a sub-rectangle of the big pathfind map.
- */
-class PathfindLayer
-{
-public:
-	PathfindLayer();
-	~PathfindLayer();
-public:
-	void reset();
-	Bool init(Bridge *theBridge, PathfindLayerEnum layer);
-	void allocateCells(const IRegion2D *extent);
-	void allocateCellsForWallLayer(const IRegion2D *extent, ObjectID *wallPieces, Int numPieces);
-	void classifyCells();
-	void classifyWallCells(ObjectID *wallPieces, Int numPieces);
-	Bool setDestroyed(Bool destroyed);
-	Bool isUnused(); // True if it doesn't contain a bridge.
-	Bool isDestroyed() {return m_destroyed;} // True if it has been destroyed.
-	PathfindCell *getCell(Int x, Int y);
-	Int getZone() {return m_zone;}
-	void setZone(Int zone) {m_zone = zone;}
-	void applyZone(); // Propagates m_zone to all cells.
-	void getStartCellIndex(ICoord2D *start) {*start = m_startCell;}
-	void getEndCellIndex(ICoord2D *end) {*end = m_endCell;}
-
-	ObjectID getBridgeID();
-	Bool connectsZones(PathfindZoneManager *zm, const LocomotorSet& locomotorSet,Int zone1, Int zone2);
-	Bool isPointOnWall(ObjectID *wallPieces, Int numPieces, const Coord3D *pt);
-
-#if defined(RTS_DEBUG)
-	void doDebugIcons() ;
-#endif
-protected:
-	void classifyLayerMapCell( Int i, Int j , PathfindCell *cell, Bridge *theBridge);
-	void classifyWallMapCell( Int i, Int j, PathfindCell *cell , ObjectID *wallPieces, Int numPieces);
-
-private:
-	PathfindCell *m_blockOfMapCells;		///< Pathfinding map - contains iconic representation of the map
-	PathfindCell **m_layerCells;		///< Pathfinding map indexes - contains matrix indexing into the map.
-	Int m_width;		// Number of cells in x
-	Int m_height;		// Number of cells in y
-	Int m_xOrigin;	// Index of first cell in x
-	Int m_yOrigin;	// Index of first cell in y
-	ICoord2D m_startCell; // pathfind cell indexes for center cell on the from side.
-	ICoord2D m_endCell; // pathfind cell indexes for center cell on the to side.
-
-	PathfindLayerEnum m_layer;
-	Int m_zone;			// Whole bridge is in same zone.
-	Bridge *m_bridge; // Corresponding bridge in TerrainLogic.
-	Bool m_destroyed;
-
-
-};
-
 
 #define PATHFIND_CELL_SIZE		10
 #define PATHFIND_CELL_SIZE_F	10.0f
