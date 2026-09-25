b152abe1a1a333fc762d63899df2b38ccaab6a96
Skyaero <21192585+Skyaero42@users.noreply.github.com>
2026-09-25 11:05:14 +0200

    refactor(pathfinder): Move Path class definition to its own file (#3334)

--- Core/GameEngine/Include/GameLogic/AIPathfind.h
+++ Core/GameEngine/Include/GameLogic/AIPathfind.h
@@ -29,12 +29,12 @@
 #pragma once
 
 #include "Common/GameType.h"
-#include "Common/GameMemory.h"
 #include "Common/Snapshot.h"
 //#include "GameLogic/Locomotor.h"	// no, do not include this, unless you like long recompiles
 #include "GameLogic/LocomotorSet.h"
 #include "GameLogic/GameLogic.h"
 
+#include "Pathfinder/Path.h"
 #include "Pathfinder/PathNode.h"
 
 class Bridge;
@@ -56,68 +56,6 @@ class PathfindCell;
 
   typedef UnsignedShort zoneStorageType;
 
-
-struct ClosestPointOnPathInfo
-{
-	Real								distAlongPath;
-	Coord3D							posOnPath;
-	PathfindLayerEnum		layer;
-};
-
-/**
- * This class encapsulates a "path" returned by the Pathfinder.
- */
-class Path : public MemoryPoolObject, public Snapshot
-{
-public:
-	Path();
-
-	PathNode *getFirstNode() { return m_path; }
-	PathNode *getLastNode() { return m_pathTail; }
-
-	void updateLastNode( const Coord3D *pos );
-
-	void prependNode( const Coord3D *pos, PathfindLayerEnum layer );				///< Create a new node at the head of the path
-	void appendNode( const Coord3D *pos, PathfindLayerEnum layer );				///< Create a new node at the end of the path
-	void setBlockedByAlly(Bool blocked) {m_blockedByAlly = blocked;}
-	Bool getBlockedByAlly() {return m_blockedByAlly;}
-	void optimize( const Object *obj, LocomotorSurfaceTypeMask acceptableSurfaces, Bool blocked );			///< Optimize the path to discard redundant nodes
-
-	void optimizeGroundPath( Bool crusher, Int diameter );			///< Optimize the ground path to discard redundant nodes
-
-	/// Given a location, return nearest location on path, and along-path dist to end as function result
-	void computePointOnPath( const Object *obj, const LocomotorSet& locomotorSet, const Coord3D& pos, ClosestPointOnPathInfo& out);
-
-	/// Given a location, return nearest location on path, and along-path dist to end as function result
-	void peekCachedPointOnPath( Coord3D& pos ) const {pos = m_cpopOut.posOnPath;}
-
-	/// Given a flight path, compute the distance to goal (0 if we are past it) & return the goal pos.
-	Real computeFlightDistToGoal( const Coord3D *pos, Coord3D& goalPos );
-
-	/// Given a location, return closest location on path, and along-path dist to end as function result
-	void markOptimized() {m_isOptimized = true;}
-
-protected:
-	// snapshot interface
-	virtual void crc( Xfer *xfer ) override;
-	virtual void xfer( Xfer *xfer ) override;
-	virtual void loadPostProcess() override;
-
-protected:
-	enum {MAX_CPOP=20};			///< Max times we will return the cached cpop.
-	MEMORY_POOL_GLUE_WITH_USERLOOKUP_CREATE( Path, "PathPool" );							///< @todo Set real numbers for mem alloc
-
-	PathNode*		m_path;															///< The list of PathNode objects that define the path
-	PathNode*		m_pathTail;
-	Bool				m_isOptimized;											///< True if the path has been optimized
-	Bool				m_blockedByAlly;										///< An ally needs to move off of this path.
-	// caching info for computePointOnPath.
-	Bool										m_cpopValid;
-	Int											m_cpopCountdown;				///< We only return the same cpop MAX_CPOP times.  It is occasionally possible to get stuck.
-	Coord3D									m_cpopIn;
-	ClosestPointOnPathInfo	m_cpopOut;
-	const PathNode*					m_cpopRecentStart;
-};
 
 //----------------------------------------------------------------------------------------------------------
 
